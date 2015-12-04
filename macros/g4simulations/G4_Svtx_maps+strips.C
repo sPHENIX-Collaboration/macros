183b91abca41eb75e3b119b8197e74c8e64018dc
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-12-04 11:11:22 -0500

    ungang S0ab strips

--- macros/g4simulations/G4_Svtx_maps+strips.C
+++ macros/g4simulations/G4_Svtx_maps+strips.C
@@ -161,8 +161,6 @@ void Svtx_Cells(int verbosity = 0)
   // strips are ganged together into a common readout channel
   PHG4SvtxAddConnectedCells *gang = new PHG4SvtxAddConnectedCells();
   gang->Verbosity(verbosity);
-  gang->set_ncells_connected(3,3);
-  gang->set_ncells_connected(4,3);
   gang->set_ncells_connected(5,3);
   gang->set_ncells_connected(6,3);
   gang->set_ncells_connected(7,6);
