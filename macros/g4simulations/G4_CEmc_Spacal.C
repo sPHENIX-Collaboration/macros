fe59e013ac2a6778cb7eb9a8fbe3d0e8fbeec350
Jin Huang <jhuang@bnl.gov>
2015-12-16 15:48:19 -0500

    Use 60ns timing window cut per decsion of HCalWorkfest III

--- macros/g4simulations/G4_CEmc_Spacal.C
+++ macros/g4simulations/G4_CEmc_Spacal.C
@@ -385,7 +385,7 @@ void CEMC_Cells(int verbosity = 0) {
           const double radius = 95;
           cemc_cells->cellsize(i,  2*TMath::Pi()/256. * radius, 2*TMath::Pi()/256. * radius);
       }
-      cemc_cells->set_timing_window_size(100);
+      cemc_cells->set_timing_window_size(60);
       se->registerSubsystem(cemc_cells);
 
     }
@@ -395,7 +395,7 @@ void CEMC_Cells(int verbosity = 0) {
       PHG4FullProjSpacalCellReco *cemc_cells = new PHG4FullProjSpacalCellReco("CEMCCYLCELLRECO");
       cemc_cells->Detector("CEMC");
       cemc_cells->Verbosity(verbosity);
-      cemc_cells->set_timing_window_size(100);
+      cemc_cells->set_timing_window_size(60);
       se->registerSubsystem(cemc_cells);
 
     }
--- macros/g4simulations/G4_HcalOut_ref.C
+++ macros/g4simulations/G4_HcalOut_ref.C
@@ -58,7 +58,7 @@ void HCALOuter_Cells(int verbosity = 0) {
   PHG4HcalCellReco *hc = new PHG4HcalCellReco();
   hc->Detector("HCALOUT");
   hc->etasize_nslat(0, 0, 5);
-  hc->set_timing_window_size(100);
+  hc->set_timing_window_size(60);
   se->registerSubsystem(hc);
 
   return;
