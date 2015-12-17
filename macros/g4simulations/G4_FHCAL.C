3de4e8de029daceff0e16e3c70f3351397275979
John Lajoie <lajoie@iastate.edu>
2015-12-17 04:30:18 -0500

    use standard area for geometry files

--- macros/g4simulations/G4_FHCAL.C
+++ macros/g4simulations/G4_FHCAL.C
@@ -35,11 +35,10 @@ FHCALSetup(PHG4Reco* g4Reco, const int absorberactive = 0)
   ostringstream mapping_hhcal;
 
   /* path to central copy of calibrations repositry */
-  // mapping_hhcal << getenv("OFFLINE_MAIN") << "/share/calibrations";
-  // mapping_hhcal << "/ForwardHcal/mapping/towerMap_FHCAL_v003.txt";
-  // cout << mapping_hhcal.str() << endl;
-
-  mapping_hhcal << "towerMap_FHCAL_latest.txt";
+  mapping_hhcal << getenv("OFFLINE_MAIN") << "/share/calibrations";
+  mapping_hhcal << "/ForwardHcal/mapping/towerMap_FHCAL_v004.txt";
+  cout << mapping_hhcal.str() << endl;
+  //mapping_hhcal << "towerMap_FHCAL_latest.txt";
 
   hhcal->SetTowerMappingFile( mapping_hhcal.str() );
   hhcal->OverlapCheck(overlapcheck);
@@ -57,9 +56,9 @@ void FHCAL_Towers(int verbosity = 0) {
   Fun4AllServer *se = Fun4AllServer::instance();
 
   ostringstream mapping_fhcal;
-  // mapping_fhcal << getenv("OFFLINE_MAIN") <<
-  // 	"/share/calibrations/ForwardHcal/mapping/towerMap_FHCAL_v003.txt";
-  mapping_fhcal << "towerMap_FHCAL_latest.txt";
+  mapping_fhcal << getenv("OFFLINE_MAIN") <<
+  	"/share/calibrations/ForwardHcal/mapping/towerMap_FHCAL_v004.txt";
+  //mapping_fhcal << "towerMap_FHCAL_latest.txt";
 
   RawTowerBuilderByHitIndex* tower_FHCAL = new RawTowerBuilderByHitIndex("TowerBuilder_FHCAL");
   tower_FHCAL->Detector("FHCAL");
@@ -68,30 +67,44 @@ void FHCAL_Towers(int verbosity = 0) {
 
   se->registerSubsystem(tower_FHCAL);
 
-  const double FHCAL_photoelectron_per_GeV = 500;
+  // const double FHCAL_photoelectron_per_GeV = 500;
 
-  RawTowerDigitizer *TowerDigitizer_FHCAL = new RawTowerDigitizer("FHCALRawTowerDigitizer");
+  // RawTowerDigitizer *TowerDigitizer_FHCAL = new RawTowerDigitizer("FHCALRawTowerDigitizer");
 
-  TowerDigitizer_FHCAL->Detector("FHCAL");
-  TowerDigitizer_FHCAL->Verbosity(verbosity);
-  TowerDigitizer_FHCAL->set_raw_tower_node_prefix("RAW");
-  TowerDigitizer_FHCAL->set_digi_algorithm(RawTowerDigitizer::kSimple_photon_digitalization);
-  TowerDigitizer_FHCAL->set_pedstal_central_ADC(0);
-  TowerDigitizer_FHCAL->set_pedstal_width_ADC(8);// eRD1 test beam setting
-  TowerDigitizer_FHCAL->set_photonelec_ADC(1);//not simulating ADC discretization error
-  TowerDigitizer_FHCAL->set_photonelec_yield_visible_GeV( FHCAL_photoelectron_per_GeV );
-  TowerDigitizer_FHCAL->set_zero_suppression_ADC(16); // eRD1 test beam setting
+  // TowerDigitizer_FHCAL->Detector("FHCAL");
+  // TowerDigitizer_FHCAL->Verbosity(verbosity);
+  // TowerDigitizer_FHCAL->set_raw_tower_node_prefix("RAW");
+  // TowerDigitizer_FHCAL->set_digi_algorithm(RawTowerDigitizer::kSimple_photon_digitalization);
+  // TowerDigitizer_FHCAL->set_pedstal_central_ADC(0);
+  // TowerDigitizer_FHCAL->set_pedstal_width_ADC(8);// eRD1 test beam setting
+  // TowerDigitizer_FHCAL->set_photonelec_ADC(1);//not simulating ADC discretization error
+  // TowerDigitizer_FHCAL->set_photonelec_yield_visible_GeV( FHCAL_photoelectron_per_GeV );
+  // TowerDigitizer_FHCAL->set_zero_suppression_ADC(16); // eRD1 test beam setting
 
-  se->registerSubsystem( TowerDigitizer_FHCAL );
+  // se->registerSubsystem( TowerDigitizer_FHCAL );
 
-  RawTowerCalibration *TowerCalibration_FHCAL = new RawTowerCalibration("FHCALRawTowerCalibration");
-  TowerCalibration_FHCAL->Detector("FHCAL");
-  TowerCalibration_FHCAL->Verbosity(verbosity);
-  TowerCalibration_FHCAL->set_calib_algorithm(RawTowerCalibration::kSimple_linear_calibration);
-  TowerCalibration_FHCAL->set_calib_const_GeV_ADC( 1. / FHCAL_photoelectron_per_GeV );
-  TowerCalibration_FHCAL->set_pedstal_ADC( 0 );
+  // RawTowerCalibration *TowerCalibration_FHCAL = new RawTowerCalibration("FHCALRawTowerCalibration");
+  // TowerCalibration_FHCAL->Detector("FHCAL");
+  // TowerCalibration_FHCAL->Verbosity(verbosity);
+  // TowerCalibration_FHCAL->set_calib_algorithm(RawTowerCalibration::kSimple_linear_calibration);
+  // TowerCalibration_FHCAL->set_calib_const_GeV_ADC( 1. / FHCAL_photoelectron_per_GeV );
+  // TowerCalibration_FHCAL->set_pedstal_ADC( 0 );
 
-  se->registerSubsystem( TowerCalibration_FHCAL );
+  // se->registerSubsystem( TowerCalibration_FHCAL );
+
+  RawTowerDigitizer *TowerDigitizer = new RawTowerDigitizer("FHCALRawTowerDigitizer");
+  TowerDigitizer->Detector("FHCAL");
+  TowerDigitizer->Verbosity(verbosity);
+  TowerDigitizer->set_digi_algorithm(RawTowerDigitizer::kNo_digitalization);
+  se->registerSubsystem( TowerDigitizer );
+
+  RawTowerCalibration *TowerCalibration = new RawTowerCalibration("FHCALRawTowerCalibration");
+  TowerCalibration->Detector("FHCAL");
+  TowerCalibration->Verbosity(verbosity);
+  TowerCalibration->set_calib_algorithm(RawTowerCalibration::kSimple_linear_calibration);
+  TowerCalibration->set_calib_const_GeV_ADC(1./0.038);
+  TowerCalibration->set_pedstal_ADC(0);
+  se->registerSubsystem( TowerCalibration );
 
 
 }
