9a11124d325aaee5849f406fa8c0c43764748605
Michael P. McCumber <michael.p.mccumber@gmail.com>
2016-04-29 14:01:44 -0600

    renaming fullmaps, adding 5 layer macro

--- macros/g4simulations/G4Setup_sPHENIX.C
+++ macros/g4simulations/G4Setup_sPHENIX.C
@@ -25,7 +25,8 @@ void G4Init(bool do_svtx = true,
       //gROOT->LoadMacro("G4_Svtx_pixels+tpc.C");    // testing
       //gROOT->LoadMacro("G4_Svtx_maps+strips.C");   // testing
       //gROOT->LoadMacro("G4_Svtx_maps+tpc.C");      // testing
-      //gROOT->LoadMacro("G4_Svtx_fullmaps.C");      // testing
+      //gROOT->LoadMacro("G4_Svtx_maps_7layers.C");  // testing
+      //gROOT->LoadMacro("G4_Svtx_maps_5layers.C");  // testing
       //gROOT->LoadMacro("G4_Svtx_ladders.C");       // testing (new geometries)
       SvtxInit();
     }
