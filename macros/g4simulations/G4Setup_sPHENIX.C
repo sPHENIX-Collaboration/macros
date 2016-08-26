1432a7403772053cfdc8c10d108dacce3b217825
Jin Huang <jhuang@bnl.gov>
2016-08-26 12:36:23 -0400

    introduce load point in setup macro. Default is not activated

--- macros/g4simulations/G4Setup_sPHENIX.C
+++ macros/g4simulations/G4Setup_sPHENIX.C
@@ -21,6 +21,7 @@ void G4Init(bool do_svtx = true,
   if (do_svtx)
     {
       gROOT->LoadMacro("G4_Svtx.C");                 // default MIE projections
+      //gROOT->LoadMacro("G4_Svtx_maps+IT+tpc.C"); // Reference design for 2016 tracking review
       //gROOT->LoadMacro("G4_Svtx_pixels+strips.C"); // testing
       //gROOT->LoadMacro("G4_Svtx_pixels+tpc.C");    // testing
       //gROOT->LoadMacro("G4_Svtx_maps+strips.C");   // testing
