35a983d1b55bc72b50246bd8beb628639ed9c838
Nils Feege <nils.feege@stonybrook.edu>
2016-12-30 13:56:23 -0500

    Add setup macro for extended IR- no functionality yet

---
+++ macros/g4simulations/G4_IR_EIC.C
@@ -0,0 +1,12 @@
+
+void IRInit() {
+  cout << "Call IR Init" << endl;
+}
+
+double IRSetup(PHG4Reco* g4Reco,
+	      const int absorberactive = 0,
+	      int verbosity = 0) {
+
+  cout << "Call IR Setup" << endl;
+  return 0;
+}
