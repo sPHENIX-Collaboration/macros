7451acbff5dbee24825d272bafe4b87d37500afc
Jin Huang <jhuang@bnl.gov>
2017-05-16 15:54:18 -0400

    Import https://wiki.bnl.gov/sPHENIX/index.php/Plot_template

---
+++ macros/sPHENIXStyle/sPhenixStyle.h
@@ -0,0 +1,22 @@
+//
+//   @file    sPhenixStyle.h         
+//   
+//            sPHENIX Style, based on a style file from ATLAS
+//
+//
+//   @author Peter Steinberg
+// 
+//   Copyright (C) 2017 sPhenix Collaboration
+//
+//   Version 0.1
+
+#ifndef  __SPHENIXSTYLE_H
+#define __SPHENIXSTYLE_H
+
+#include "TStyle.h"
+
+void SetsPhenixStyle();
+
+TStyle* sPhenixStyle(); 
+
+#endif // __SPHENIXSTYLE_H
