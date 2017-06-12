7760be9ba0523cd7c650298e64a6803821c40926
Jin Huang <blackcathj@gmail.com>
2017-06-12 14:38:44 -0400

    Allow switch to use 40-layer TPC for tracking team tests

--- macros/g4simulations/G4Setup_sPHENIX.C
+++ macros/g4simulations/G4Setup_sPHENIX.C
@@ -20,7 +20,8 @@ void G4Init(bool do_svtx = true,
     }  
   if (do_svtx)
     {
-      gROOT->LoadMacro("G4_Svtx_maps_ladders+intt_ladders+tpc_KalmanPatRec.C"); 
+      gROOT->LoadMacro("G4_Svtx_maps_ladders+intt_ladders+tpc_KalmanPatRec.C");  // default tracker setup with laddered silicon trackers and 60-layer TPC (approximation of final design)
+      // gROOT->LoadMacro("G4_Svtx_maps_ladders+intt_ladders+tpc_40layer_KalmanPatRec.C"); // 40-layer TPC under test by tracking team
       SvtxInit();
     }
 
