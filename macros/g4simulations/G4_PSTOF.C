a39ab260293566966090ed0918a7d3e8d1162435
Chris Pinkenburg <pinkenburg@bnl.gov>
2017-08-03 16:29:29 -0400

    fix PSTOF macro

--- macros/g4simulations/G4_PSTOF.C
+++ macros/g4simulations/G4_PSTOF.C
@@ -8,33 +8,20 @@
  * \date $Date$
  */
 
-#include <cmath>
-
 void PSTOFInit()
-{
-  std::cout << "In PSTOFInit()" << std::endl;
-}
+{}
 
 double PSTOF(PHG4Reco* g4Reco, double radius, const int absorberactive = 0)
 {
-  std::cout << "In PSTOF" << std::endl;
-
   gSystem->Load("libg4detectors.so");
   gSystem->Load("libg4testbench.so");
   
-  /*
-  const double psTOF_R = 85.0;
-  const double length = 2*74.0;
-  const double z_start = 0.5*length;
-  const double z_end = -0.5*length;
-  */
 
   PHG4PSTOFSubsystem *pstof = new PHG4PSTOFSubsystem( "PSTOF" );
-  // pstof->set_int_param("active",1);
-  // pstof->set_int_param("use_g4steps",0);
-  //pstof->OverlapCheck( overlapcheck );
- 
+  pstof->SuperDetector("PSTOF");
+//  pstof->SetActive(-1,1); // set all modules active
   g4Reco->registerSubsystem( pstof );
 
-  return 90.; // returns the radius??
+// returns the outer radius to check for overlaps with next detector
+  return 90.; 
 }
