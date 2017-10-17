1a9a90f1a6c04b41a5ca6173aaf11260fd62de45
Jin Huang <jhuang@bnl.gov>
2017-10-16 23:55:45 -0400

    use default embedding ID

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -429,7 +429,7 @@ int Fun4All_G4_sPHENIX(
       //! positive ID is the embedded event of interest, e.g. jetty event from pythia
       //! negative IDs are backgrounds, .e.g out of time pile up collisions
       //! Usually, ID = 0 means the primary Au+Au collision background
-      in->set_embedding_id(2);
+      //in->set_embedding_id(2);
     }
   else
     {
