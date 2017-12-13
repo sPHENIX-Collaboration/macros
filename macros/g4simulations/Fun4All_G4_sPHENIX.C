675f5bdb03f4bdb652560f3017b99a2f1558d890
Jin Huang <blackcathj@gmail.com>
2017-12-13 10:42:35 -0500

    Comment of optional vertex distribution in hepmc inputs

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -443,8 +443,10 @@ int Fun4All_G4_sPHENIX(
     Fun4AllHepMCInputManager *in = new Fun4AllHepMCInputManager("HepMCInput_1");
     se->registerInputManager(in);
     se->fileopen(in->Name().c_str(), inputFile);
-    //in->set_vertex_distribution_width(100e-4,100e-4,30,0);//optional collision smear in space time
-    //in->set_vertex_distribution_mean(0,0,1,0);//optional collision central position shift in space time
+    //in->set_vertex_distribution_width(100e-4,100e-4,30,0);//optional collision smear in space, time
+    //in->set_vertex_distribution_mean(0,0,1,0);//optional collision central position shift in space, time
+    // //optional choice of vertex distribution function in space, time
+    //in->set_vertex_distribution_function(PHHepMCGenHelper::Gaus,PHHepMCGenHelper::Gaus,PHHepMCGenHelper::Uniform,PHHepMCGenHelper::Gaus);
     //! embedding ID for the event
     //! positive ID is the embedded event of interest, e.g. jetty event from pythia
     //! negative IDs are backgrounds, .e.g out of time pile up collisions
