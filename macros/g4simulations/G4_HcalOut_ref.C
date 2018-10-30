322f945b4818150982ca87b02932edb45881967d
Chris Pinkenburg <pinkenburg@bnl.gov>
2018-10-30 16:44:24 -0400

    Add root6 compliant macros

--- macros/g4simulations/G4_HcalOut_ref.C
+++ macros/g4simulations/G4_HcalOut_ref.C
@@ -1,4 +1,21 @@
-
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include "GlobalVariables.C"
+#include <caloreco/RawClusterBuilderGraph.h>
+#include <caloreco/RawClusterBuilderTemplate.h>
+#include <caloreco/RawTowerCalibration.h>
+#include <fun4all/Fun4AllServer.h>
+#include <g4calo/HcalRawTowerBuilder.h>
+#include <g4calo/RawTowerDigitizer.h>
+#include <g4detectors/PHG4OuterHcalSubsystem.h>
+#include <g4detectors/PHG4HcalCellReco.h>
+#include <g4eval/CaloEvaluator.h>
+#include <g4main/PHG4Reco.h>
+R__LOAD_LIBRARY(libcalo_reco.so)
+R__LOAD_LIBRARY(libg4calo.so)
+R__LOAD_LIBRARY(libg4detectors.so)
+R__LOAD_LIBRARY(libg4eval.so)
+#endif
 
 enum enu_HCalOut_clusterizer
 {
