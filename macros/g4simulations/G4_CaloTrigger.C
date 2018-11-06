5216ae680fe9464e9947422e32c9a71a9ae78254
Chris Pinkenburg <pinkenburg@bnl.gov>
2018-11-06 15:17:13 -0500

    root5/6 identical

--- macros/g4simulations/G4_CaloTrigger.C
+++ macros/g4simulations/G4_CaloTrigger.C
@@ -1,3 +1,11 @@
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <fun4all/Fun4AllServer.h>
+#include <calotrigger/CaloTriggerSim.h>
+R__LOAD_LIBRARY(libcalotrigger.so)
+#endif
+
+void CaloTriggerInit() {}
 
 void CaloTrigger_Sim(int verbosity = 0) {
   
