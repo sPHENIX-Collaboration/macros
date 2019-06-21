d4c911e1d3d5cfed7bc4340677ff87159d37f3ac
Anthony Denis Frawley <afrawley@fsu.edu>
2019-06-20 21:54:44 -0400

    Set default number of particles back to 1

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -234,8 +234,8 @@ int Fun4All_G4_sPHENIX(
     {
       // toss low multiplicity dummy events
       PHG4SimpleEventGenerator *gen = new PHG4SimpleEventGenerator();
-      //gen->add_particles("pi-", 1);  // mu+,e+,proton,pi+,Upsilon
-      gen->add_particles("pi+",100); // 100 pion option
+      gen->add_particles("pi-", 1);  // mu+,e+,proton,pi+,Upsilon
+      //gen->add_particles("pi+",100); // 100 pion option
       if (readhepmc || do_embedding || runpythia8 || runpythia6)
       {
         gen->set_reuse_existing_vertex(true);
