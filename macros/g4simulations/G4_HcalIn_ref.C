9737d6225d9c477b5110041401dcdd39021c1137
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-09-10 21:54:10 -0400

    set material to SS310 if Al is set to false

--- macros/g4simulations/G4_HcalIn_ref.C
+++ macros/g4simulations/G4_HcalIn_ref.C
@@ -65,10 +65,15 @@ double HCalInner(PHG4Reco* g4Reco,
   // these are the parameters you can change with their default settings
   // hcal->set_string_param("material","SS310");
   if(inner_hcal_material_Al)
-    {
-      cout <<"HCalInner - construct inner HCal absorber with G4_Al"<<endl;
-      hcal->set_string_param("material","G4_Al");
-    }
+  {
+    cout <<"HCalInner - construct inner HCal absorber with G4_Al"<<endl;
+    hcal->set_string_param("material","G4_Al");
+  }
+  else
+  {
+    cout <<"HCalInner - construct inner HCal absorber with SS310"<<endl;
+    hcal->set_string_param("material","SS310");
+  }
   // hcal->set_double_param("inner_radius", 117.27);
   //-----------------------------------------
   // the light correction can be set in a single call
