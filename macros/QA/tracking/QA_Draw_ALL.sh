5bcf3b1b56dfd29af29269072d9dabbd283564d7
Jin Huang <jhuang@bnl.gov>
2020-04-23 20:25:06 -0400

    correct module name

--- macros/QA/tracking/QA_Draw_ALL.sh
+++ macros/QA/tracking/QA_Draw_ALL.sh
@@ -21,7 +21,7 @@ echo "$0 - New QA file: $new_QA_file";
 echo "$0 - Reference QA file: $reference_QA_file";
 
 # mvtx stuff
-root -b -q "QA_Draw_Mvtx.C(${q}QAG4SimulationUpsilon${q},$new_QA_file, $reference_QA_file)"
+root -b -q "QA_Draw_Mvtx.C(${q}QAG4SimulationMvtx${q},$new_QA_file, $reference_QA_file)"
 
 # last all jet stuff
 root -b -q "QA_Draw_Tracking_TruthMatchingOverview.C(${q}QAG4SimulationTracking${q}, $new_QA_file, $reference_QA_file)"
