a572682f2c7dde688f736d5537a0bb4b835cada3
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-01 11:23:04 -0400

    add max radius for black hole so all detectors can update it, return code only works for barrel

--- macros/g4simulations/GlobalVariables.C
+++ macros/g4simulations/GlobalVariables.C
@@ -2,4 +2,8 @@
 #if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
 static bool overlapcheck = false;
 static double no_overlapp = 0.0001; // added to radii to avoid overlapping volumes
+namespace BlackHoleGeometry
+{
+  static double max_radius = 0.; // this is needed for the overall dimension of the black hole
+};
 #endif
