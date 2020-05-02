5878854b525448685299a9abd63cad2c47b5efcc
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 00:11:59 -0400

    remove printouts

--- macros/g4simulations/G4_WorldSize.C
+++ macros/g4simulations/G4_WorldSize.C
@@ -1,9 +1,10 @@
 #pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+
 #include "GlobalVariables.C"
 #include <g4main/PHG4Reco.h>
+
 R__LOAD_LIBRARY(libg4testbench.so)
-#endif
+
 
 void WorldSize(PHG4Reco *g4Reco, double radius)
 {
@@ -18,12 +19,9 @@ void WorldSize(PHG4Reco *g4Reco, double radius)
   }
 // our world is symmetric, pick the largest of abs(min_z) || abs(max_z)
   double max_zval = std::max(fabs(BlackHoleGeometry::min_z),fabs(BlackHoleGeometry::max_z));
-  cout << "max_zval: " << max_zval << endl;
   if (g4Reco->GetWorldSizeZ() < 2*(max_zval+100))
   {
     g4Reco->SetWorldSizeZ( 2*(max_zval+100));
   }
-  cout << "worldsize X: " << g4Reco->GetWorldSizeX() << endl;
-  cout <<  "worldsize Z: " << g4Reco->GetWorldSizeZ() << endl;
   return;
 }
