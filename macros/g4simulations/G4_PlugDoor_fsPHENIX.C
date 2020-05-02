2be05bf48b5e85d71a4afeaa9e77d50d4449cffb
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 00:04:42 -0400

    fix the dimensions to be exact for a tight fit of the black hole

--- macros/g4simulations/G4_PlugDoor_fsPHENIX.C
+++ macros/g4simulations/G4_PlugDoor_fsPHENIX.C
@@ -1,16 +1,30 @@
 #pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 00, 0)
+
+#include "GlobalVariables.C"
+
 #include <g4detectors/PHG4CylinderSubsystem.h>
 #include <g4main/PHG4Reco.h>
-#include "GlobalVariables.C"
+
 R__LOAD_LIBRARY(libg4detectors.so)
-#endif
+
+const double place_z = 335.9;
+const double length = 10.2;
+const double inner_radius = 2.1;
+const double thickness = 258.5;
 
 void PlugDoorInit()
 {
-  if (BlackHoleGeometry::max_radius < 263.5)
+  if (BlackHoleGeometry::max_radius < (inner_radius+thickness))
   {
-    BlackHoleGeometry::max_radius = 263.5;  // eye balled, it can shrink a bit
+    BlackHoleGeometry::max_radius = inner_radius+thickness;
+  }
+  if (BlackHoleGeometry::max_z < (place_z + length/2.))
+  {
+    BlackHoleGeometry::max_z = place_z + length/2.;
+  }
+  if (BlackHoleGeometry::min_z > -(place_z + length/2.))
+  {
+    BlackHoleGeometry::min_z = -(place_z + length/2.);
   }
 }
 
@@ -24,11 +38,10 @@ void PlugDoor(PHG4Reco *g4Reco,
   const int flux_door_active = false;
 
   PHG4CylinderSubsystem *flux_return_plus = new PHG4CylinderSubsystem("FWDFLUXRET", 0);
-  flux_return_plus->set_int_param("lengthviarapidity", 0);
-  flux_return_plus->set_double_param("length", 10.2);
-  flux_return_plus->set_double_param("radius", 2.1);
-  flux_return_plus->set_double_param("place_z", 335.9);
-  flux_return_plus->set_double_param("thickness", 263.5 - 5.0);
+  flux_return_plus->set_double_param("length", length);
+  flux_return_plus->set_double_param("radius", inner_radius);
+  flux_return_plus->set_double_param("place_z", place_z);
+  flux_return_plus->set_double_param("thickness", thickness);
   flux_return_plus->set_string_param("material", material);
   flux_return_plus->SetActive(flux_door_active);
   flux_return_plus->SuperDetector("FLUXRET_ETA_PLUS");
@@ -36,11 +49,10 @@ void PlugDoor(PHG4Reco *g4Reco,
   g4Reco->registerSubsystem(flux_return_plus);
 
   PHG4CylinderSubsystem *flux_return_minus = new PHG4CylinderSubsystem("FWDFLUXRET", 1);
-  flux_return_minus->set_int_param("lengthviarapidity", 0);
-  flux_return_minus->set_double_param("length", 10.2);
-  flux_return_minus->set_double_param("radius", 2.1);
-  flux_return_minus->set_double_param("place_z", -335.9);
-  flux_return_minus->set_double_param("thickness", 263.5 - 5.0);
+  flux_return_minus->set_double_param("length", length);
+  flux_return_minus->set_double_param("radius", inner_radius);
+  flux_return_minus->set_double_param("place_z", -place_z);
+  flux_return_minus->set_double_param("thickness", thickness);
   flux_return_minus->set_string_param("material", material);
   flux_return_minus->SetActive(flux_door_active);
   flux_return_minus->SuperDetector("FLUXRET_ETA_MINUS");
