e5f790de7217204695aec628891fb342bccdc08c
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 19:47:19 -0400

    clang-format

--- macros/g4simulations/G4_PlugDoor_EIC.C
+++ macros/g4simulations/G4_PlugDoor_EIC.C
@@ -7,29 +7,28 @@
 
 R__LOAD_LIBRARY(libg4detectors.so)
 
-
 namespace PlugDoorEicMacro
 {
-  // sPHENIX forward flux return(s)
-  // define via four corners in the engineering drawing
-const double z_1  = 330.81;
-const double z_2  = 360.81;
-const double r_1  = 30;
-const double r_2  = 263.5;
+// sPHENIX forward flux return(s)
+// define via four corners in the engineering drawing
+const double z_1 = 330.81;
+const double z_2 = 360.81;
+const double r_1 = 30;
+const double r_2 = 263.5;
 
 const double length = z_2 - z_1;
-const double place_z =  -(z_1 + z_2) / 2.;
-}
+const double place_z = -(z_1 + z_2) / 2.;
+}  // namespace PlugDoorEicMacro
 
-void PlugDoorInit() 
+void PlugDoorInit()
 {
   if (BlackHoleGeometry::max_radius < PlugDoorEicMacro::r_2)
   {
     BlackHoleGeometry::max_radius = PlugDoorEicMacro::r_2;
   }
-  if (BlackHoleGeometry::min_z > (PlugDoorEicMacro::place_z - PlugDoorEicMacro::length/2.))
+  if (BlackHoleGeometry::min_z > (PlugDoorEicMacro::place_z - PlugDoorEicMacro::length / 2.))
   {
-    BlackHoleGeometry::min_z = (PlugDoorEicMacro::place_z - PlugDoorEicMacro::length/2.);
+    BlackHoleGeometry::min_z = (PlugDoorEicMacro::place_z - PlugDoorEicMacro::length / 2.);
   }
 }
 
@@ -49,7 +48,7 @@ void PlugDoor(PHG4Reco *g4Reco,
   flux_return_minus->set_double_param("thickness", PlugDoorEicMacro::r_2 - PlugDoorEicMacro::r_1);
   flux_return_minus->set_string_param("material", material);
   flux_return_minus->SetActive(flux_door_active);
-//  flux_return_minus->SuperDetector("FLUXRET_ETA_MINUS");
+  //  flux_return_minus->SuperDetector("FLUXRET_ETA_MINUS");
   flux_return_minus->OverlapCheck(overlapcheck);
   g4Reco->registerSubsystem(flux_return_minus);
 
