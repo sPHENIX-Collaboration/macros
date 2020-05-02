d5265f1f82a16ae01f54b85421adada904738335
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 19:47:02 -0400

    use namespace for global variables - the file is included, they caollide all over the place

--- macros/g4simulations/G4_PlugDoor_EIC.C
+++ macros/g4simulations/G4_PlugDoor_EIC.C
@@ -8,6 +8,8 @@
 R__LOAD_LIBRARY(libg4detectors.so)
 
 
+namespace PlugDoorEicMacro
+{
   // sPHENIX forward flux return(s)
   // define via four corners in the engineering drawing
 const double z_1  = 330.81;
@@ -17,16 +19,17 @@ const double r_2  = 263.5;
 
 const double length = z_2 - z_1;
 const double place_z =  -(z_1 + z_2) / 2.;
+}
 
 void PlugDoorInit() 
 {
-  if (BlackHoleGeometry::max_radius < r_2)
+  if (BlackHoleGeometry::max_radius < PlugDoorEicMacro::r_2)
   {
-    BlackHoleGeometry::max_radius = r_2;
+    BlackHoleGeometry::max_radius = PlugDoorEicMacro::r_2;
   }
-  if (BlackHoleGeometry::min_z > (place_z - length/2.))
+  if (BlackHoleGeometry::min_z > (PlugDoorEicMacro::place_z - PlugDoorEicMacro::length/2.))
   {
-    BlackHoleGeometry::min_z = (place_z - length/2.);
+    BlackHoleGeometry::min_z = (PlugDoorEicMacro::place_z - PlugDoorEicMacro::length/2.);
   }
 }
 
@@ -40,10 +43,10 @@ void PlugDoor(PHG4Reco *g4Reco,
 
   PHG4CylinderSubsystem *flux_return_minus = new PHG4CylinderSubsystem("FLUXRET_ETA_MINUS", 0);
   flux_return_minus->set_int_param("lengthviarapidity", 0);
-  flux_return_minus->set_double_param("length", length);
-  flux_return_minus->set_double_param("radius", r_1);
-  flux_return_minus->set_double_param("place_z", place_z);
-  flux_return_minus->set_double_param("thickness", r_2 - r_1);
+  flux_return_minus->set_double_param("length", PlugDoorEicMacro::length);
+  flux_return_minus->set_double_param("radius", PlugDoorEicMacro::r_1);
+  flux_return_minus->set_double_param("place_z", PlugDoorEicMacro::place_z);
+  flux_return_minus->set_double_param("thickness", PlugDoorEicMacro::r_2 - PlugDoorEicMacro::r_1);
   flux_return_minus->set_string_param("material", material);
   flux_return_minus->SetActive(flux_door_active);
 //  flux_return_minus->SuperDetector("FLUXRET_ETA_MINUS");
