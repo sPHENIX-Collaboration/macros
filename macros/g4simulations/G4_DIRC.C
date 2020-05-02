85c20deabce11fbad07e616ff42014a611e4b044
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 10:26:34 -0400

    clang-format

--- macros/g4simulations/G4_DIRC.C
+++ macros/g4simulations/G4_DIRC.C
@@ -1,11 +1,15 @@
 #pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+
 #include "GlobalVariables.C"
+
 #include <g4detectors/PHG4CylinderSubsystem.h>
 #include <g4detectors/PHG4SectorSubsystem.h>
 #include <g4main/PHG4Reco.h>
+
+#include <cmath>
+
 R__LOAD_LIBRARY(libg4detectors.so)
-#endif
+
 // $Id: G4_DIRC.C,v 1.3 2013/10/09 01:08:17 jinhuang Exp $
 
 /*!
@@ -16,23 +20,21 @@ R__LOAD_LIBRARY(libg4detectors.so)
  * \date $Date: 2013/10/09 01:08:17 $
  */
 
-#include <cmath>
 namespace DIRC
 {
 const double radiator_R = 83.65;
-  const double length = 400;
-  const double z_shift = -75; //115
-  const double z_start = z_shift + length / 2.;
-  const double z_end = z_shift - length / 2.;
-  const double outer_skin_radius = 89.25;
-}
+const double length = 400;
+const double z_shift = -75;  //115
+const double z_start = z_shift + length / 2.;
+const double z_end = z_shift - length / 2.;
+const double outer_skin_radius = 89.25;
+}  // namespace DIRC
 
-void
-DIRCInit()
+void DIRCInit()
 {
   if (BlackHoleGeometry::max_radius < DIRC::outer_skin_radius)
   {
-    BlackHoleGeometry::max_radius = DIRC::outer_skin_radius; // from default in code - needs changing
+    BlackHoleGeometry::max_radius = DIRC::outer_skin_radius;  // from default in code - needs changing
   }
   if (BlackHoleGeometry::max_z < DIRC::z_start)
   {
@@ -42,19 +44,16 @@ DIRCInit()
   {
     BlackHoleGeometry::min_z = DIRC::z_end;
   }
-
 }
 
 //! Babar DIRC (Without most of support structure)
 //! Ref: I. Adam et al. The DIRC particle identification system for the BaBar experiment.
 //! Nucl. Instrum. Meth., A538:281-357, 2005. doi:10.1016/j.nima.2004.08.129.
-double
-DIRCSetup(PHG4Reco* g4Reco)
+double DIRCSetup(PHG4Reco *g4Reco)
 {
-
   PHG4SectorSubsystem *dirc;
   dirc = new PHG4SectorSubsystem("DIRC");
-  dirc->get_geometry().set_normal_polar_angle(M_PI/2);
+  dirc->get_geometry().set_normal_polar_angle(M_PI / 2);
   dirc->get_geometry().set_normal_start(83.65 * PHG4Sector::Sector_Geometry::Unit_cm());
   dirc->get_geometry().set_min_polar_angle(atan2(DIRC::radiator_R, DIRC::z_start));
   dirc->get_geometry().set_max_polar_angle(atan2(DIRC::radiator_R, DIRC::z_end));
@@ -75,13 +74,13 @@ DIRCSetup(PHG4Reco* g4Reco)
 
   // Inner skin:
   cyl = new PHG4CylinderSubsystem("DIRC_CST_Inner_Skin", 10);
-  cyl->set_double_param("radius",81.71);
-  cyl->set_double_param("length",DIRC::length);
-  cyl->set_string_param("material","G4_Al");
-  cyl->set_double_param("thickness",0.127);
-  cyl->set_double_param("place_x",0.);
-  cyl->set_double_param("place_y",0.);
-  cyl->set_double_param("place_z",DIRC::z_shift);
+  cyl->set_double_param("radius", 81.71);
+  cyl->set_double_param("length", DIRC::length);
+  cyl->set_string_param("material", "G4_Al");
+  cyl->set_double_param("thickness", 0.127);
+  cyl->set_double_param("place_x", 0.);
+  cyl->set_double_param("place_y", 0.);
+  cyl->set_double_param("place_z", DIRC::z_shift);
   cyl->SetActive(0);
   cyl->SuperDetector("DIRC");
   cyl->OverlapCheck(overlapcheck);
@@ -90,13 +89,13 @@ DIRCSetup(PHG4Reco* g4Reco)
 
   // Outer skin:
   cyl = new PHG4CylinderSubsystem("DIRC_CST_Outer_Skin", 11);
-  cyl->set_double_param("radius",DIRC::outer_skin_radius - 0.076);
-  cyl->set_double_param("length",DIRC::length);
-  cyl->set_string_param("material","G4_Al");
-  cyl->set_double_param("thickness",0.076);
-  cyl->set_double_param("place_x",0.);
-  cyl->set_double_param("place_y",0.);
-  cyl->set_double_param("place_z",DIRC::z_shift);
+  cyl->set_double_param("radius", DIRC::outer_skin_radius - 0.076);
+  cyl->set_double_param("length", DIRC::length);
+  cyl->set_string_param("material", "G4_Al");
+  cyl->set_double_param("thickness", 0.076);
+  cyl->set_double_param("place_x", 0.);
+  cyl->set_double_param("place_y", 0.);
+  cyl->set_double_param("place_z", DIRC::z_shift);
   cyl->SetActive(0);
   cyl->SuperDetector("DIRC");
   cyl->OverlapCheck(overlapcheck);
@@ -105,5 +104,4 @@ DIRCSetup(PHG4Reco* g4Reco)
 
   // Done
   return 89.25;
-
 }
