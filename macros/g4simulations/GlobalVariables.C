2b852e08b8b165b47aeb2949ef3fcf875d0faef1
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 00:09:09 -0400

    add Enable namespace for future enable flags, add parameters for black hole dimensions

--- macros/g4simulations/GlobalVariables.C
+++ macros/g4simulations/GlobalVariables.C
@@ -1,9 +1,18 @@
 #pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
 static bool overlapcheck = false;
 static double no_overlapp = 0.0001; // added to radii to avoid overlapping volumes
+
+namespace Enable
+{
+  static bool BlackHole = false;
+  static bool OverLapCheck = false;
+}
+
 namespace BlackHoleGeometry
 {
   static double max_radius = 0.; // this is needed for the overall dimension of the black hole
+  static double min_z = 0.;
+  static double max_z = 0.;
+  static double gap = no_overlapp;
+  static bool visible = false;
 };
-#endif
