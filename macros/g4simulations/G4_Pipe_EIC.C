45d5e1ec74dc846c78e4a45ab9fc96dcfde1e3cb
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-02 22:01:37 -0400

    use min/max for black hole params

--- macros/g4simulations/G4_Pipe_EIC.C
+++ macros/g4simulations/G4_Pipe_EIC.C
@@ -1,15 +1,34 @@
 #pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 00, 0)
-#include <TSystem.h>
+
+#include "GlobalVariables.C"
+
 #include <g4detectors/PHG4CylinderSubsystem.h>
 #include <g4detectors/PHG4GDMLSubsystem.h>
+
 #include <g4main/PHG4Reco.h>
-#include "GlobalVariables.C"
+
+#include <TSystem.h>
+
 R__LOAD_LIBRARY(libg4detectors.so)
-#endif
+
+namespace PipeEicMacro
+{
+  // Central pipe dimension
+  // Extracted via mechanical model: Detector chamber 3-20-20
+  // directly implimenting the central Be section in G4 cylinder for max speed simulation in the detector region.
+  // The jointer lip structure of the pipe R = 3.2cm x L=5mm is ignored here
+  static const double be_pipe_radius = 3.1000;
+  static const double be_pipe_thickness = 3.1762 - be_pipe_radius;  // 760 um for sPHENIX
+  static const double be_pipe_length_plus = 66.8;                   // +z beam pipe extend.
+  static const double be_pipe_length_neg = -79.8;                   // -z beam pipe extend.
+}
 
 void PipeInit()
 {
+BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, PipeEicMacro::be_pipe_radius + PipeEicMacro::be_pipe_thickness);
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, PipeEicMacro::be_pipe_length_plus);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, PipeEicMacro::be_pipe_length_neg);
+
 }
 
 //! construct beam pipe
@@ -25,22 +44,14 @@ double Pipe(PHG4Reco* g4Reco,
   const static bool do_pipe_hadron_forward_extension = use_forward_pipes && true;
   const static bool do_pipe_electron_forward_extension = use_forward_pipes && true;
 
-  // Central pipe dimension
-  // Extracted via mechanical model: Detector chamber 3-20-20
-  // directly implimenting the central Be section in G4 cylinder for max speed simulation in the detector region.
-  // The jointer lip structure of the pipe R = 3.2cm x L=5mm is ignored here
-  static const double be_pipe_radius = 3.1000;
-  static const double be_pipe_thickness = 3.1762 - be_pipe_radius;  // 760 um for sPHENIX
-  static const double be_pipe_length_plus = 66.8;                   // +z beam pipe extend.
-  static const double be_pipe_length_neg = -79.8;                   // -z beam pipe extend.
 
-  static const double be_pipe_length = be_pipe_length_plus - be_pipe_length_neg;  // pipe length
-  static const double be_pipe_center = 0.5 * (be_pipe_length_plus + be_pipe_length_neg);
+  static const double be_pipe_length = PipeEicMacro::be_pipe_length_plus - PipeEicMacro::be_pipe_length_neg;  // pipe length
+  static const double be_pipe_center = 0.5 * (PipeEicMacro::be_pipe_length_plus + PipeEicMacro::be_pipe_length_neg);
 
-  if (radius > be_pipe_radius)
+  if (radius > PipeEicMacro::be_pipe_radius)
   {
     cout << "inconsistency: radius: " << radius
-         << " larger than pipe inner radius: " << be_pipe_radius << endl;
+         << " larger than pipe inner radius: " << PipeEicMacro::be_pipe_radius << endl;
     gSystem->Exit(-1);
   }
 
@@ -54,25 +65,25 @@ double Pipe(PHG4Reco* g4Reco,
   cyl->set_double_param("length", be_pipe_length);
   cyl->set_double_param("place_z", be_pipe_center);
   cyl->set_string_param("material", "G4_Galactic");
-  cyl->set_double_param("thickness", be_pipe_radius);
+  cyl->set_double_param("thickness", PipeEicMacro::be_pipe_radius);
   cyl->SuperDetector("PIPE");
   cyl->OverlapCheck(overlapcheck);
   if (absorberactive) cyl->SetActive();
   g4Reco->registerSubsystem(cyl);
 
   cyl = new PHG4CylinderSubsystem("BE_PIPE", 1);
-  cyl->set_double_param("radius", be_pipe_radius);
+  cyl->set_double_param("radius", PipeEicMacro::be_pipe_radius);
   cyl->set_int_param("lengthviarapidity", 0);
   cyl->set_double_param("length", be_pipe_length);
   cyl->set_double_param("place_z", be_pipe_center);
   cyl->set_string_param("material", "G4_Be");
-  cyl->set_double_param("thickness", be_pipe_thickness);
+  cyl->set_double_param("thickness", PipeEicMacro::be_pipe_thickness);
   cyl->SuperDetector("PIPE");
   cyl->OverlapCheck(overlapcheck);
   if (absorberactive) cyl->SetActive();
   g4Reco->registerSubsystem(cyl);
 
-  radius = be_pipe_radius + be_pipe_thickness;
+  radius = PipeEicMacro::be_pipe_radius + PipeEicMacro::be_pipe_thickness;
 
   radius += no_overlapp;
 
