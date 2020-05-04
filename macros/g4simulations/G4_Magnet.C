c9540f9080d9134c4099f881982a6aac414ac3f5
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-04 15:46:54 -0400

    add babar magnet namespace

--- macros/g4simulations/G4_Magnet.C
+++ macros/g4simulations/G4_Magnet.C
@@ -1,12 +1,26 @@
 #pragma once
-#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
-#include "GlobalVariables.C"
-#include <g4detectors/PHG4CylinderSubsystem.h>
-#include <g4main/PHG4Reco.h>
-R__LOAD_LIBRARY(libg4detectors.so)
-#endif
 
-void MagnetInit() {}
+#include "GlobalVariables.C"
+
+#include <g4detectors/PHG4CylinderSubsystem.h>
+
+#include <g4main/PHG4Reco.h>
+
+R__LOAD_LIBRARY(libg4detectors.so)
+
+namespace BabarMagnetMacro
+{
+static double magnet_outer_cryostat_wall_radius = 174.5;
+static  double magnet_outer_cryostat_wall_thickness = 2.5;
+static  double magnet_length = 379.;
+}
+
+void MagnetInit() 
+{
+  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, BabarMagnetMacro::magnet_outer_cryostat_wall_radius+BabarMagnetMacro::magnet_outer_cryostat_wall_thickness);
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, BabarMagnetMacro::magnet_length/2.);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, -BabarMagnetMacro::magnet_length/2.);
+}
 
 double Magnet(PHG4Reco* g4Reco,
 	      double radius,
@@ -16,8 +30,6 @@ double Magnet(PHG4Reco* g4Reco,
 
   double magnet_inner_cryostat_wall_radius = 142;
   double magnet_inner_cryostat_wall_thickness = 1;
-  double magnet_outer_cryostat_wall_radius = 174.5;
-  double magnet_outer_cryostat_wall_thickness = 2.5;
   double magnet_coil_radius = 150.8;
   double magnet_coil_thickness = 9.38;
   double magnet_length = 379.;
@@ -35,7 +47,7 @@ double Magnet(PHG4Reco* g4Reco,
   PHG4CylinderSubsystem *cyl = new PHG4CylinderSubsystem("MAGNET", 0);
   cyl->set_double_param("radius",magnet_inner_cryostat_wall_radius);
   cyl->set_int_param("lengthviarapidity",0);
-  cyl->set_double_param("length",magnet_length);
+  cyl->set_double_param("length",BabarMagnetMacro::magnet_length);
 cyl->set_double_param("thickness",magnet_inner_cryostat_wall_thickness);
 cyl->set_string_param("material","Al5083"); // use 1 radiation length Al for magnet thickness
   cyl->SuperDetector("MAGNET");
@@ -53,23 +65,23 @@ cyl->set_string_param("material","Al5083"); // use 1 radiation length Al for mag
   g4Reco->registerSubsystem( cyl );
 
   cyl = new PHG4CylinderSubsystem("MAGNET", 2);
-  cyl->set_double_param("radius",magnet_outer_cryostat_wall_radius);
+  cyl->set_double_param("radius",BabarMagnetMacro::magnet_outer_cryostat_wall_radius);
   cyl->set_int_param("lengthviarapidity",0);
-  cyl->set_double_param("length",magnet_length);
-  cyl->set_double_param("thickness",magnet_outer_cryostat_wall_thickness);
+  cyl->set_double_param("length",BabarMagnetMacro::magnet_length);
+  cyl->set_double_param("thickness",BabarMagnetMacro::magnet_outer_cryostat_wall_thickness);
   cyl->set_string_param("material","Al5083"); // use 1 radiation length Al for magnet thickness
   cyl->SuperDetector("MAGNET");
   if (absorberactive)  cyl->SetActive();
   g4Reco->registerSubsystem( cyl );
 
-  radius = magnet_outer_cryostat_wall_radius + magnet_outer_cryostat_wall_thickness; // outside of magnet
+  radius = BabarMagnetMacro::magnet_outer_cryostat_wall_radius + BabarMagnetMacro::magnet_outer_cryostat_wall_thickness; // outside of magnet
   
   if (verbosity > 0) {
     cout << "========================= G4_Magnet.C::Magnet() ===========================" << endl;
     cout << " MAGNET Material Description:" << endl;
     cout << "  inner radius = " << magnet_inner_cryostat_wall_radius << " cm" << endl;
-    cout << "  outer radius = " << magnet_outer_cryostat_wall_radius + magnet_outer_cryostat_wall_thickness << " cm" << endl;
-    cout << "  length = " << magnet_length << " cm" << endl;
+    cout << "  outer radius = " << BabarMagnetMacro::magnet_outer_cryostat_wall_radius + BabarMagnetMacro::magnet_outer_cryostat_wall_thickness << " cm" << endl;
+    cout << "  length = " << BabarMagnetMacro::magnet_length << " cm" << endl;
     cout << "===========================================================================" << endl;
   }
 
