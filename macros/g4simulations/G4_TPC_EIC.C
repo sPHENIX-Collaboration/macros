4fcc84006fb281f81d2f043134ad04dd811df5f3
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-13 15:31:30 -0400

    cleanup

--- macros/g4simulations/G4_TPC_EIC.C
+++ macros/g4simulations/G4_TPC_EIC.C
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "GlobalVariables.C"
 #include "G4_Mvtx.C"
+#include "GlobalVariables.C"
 
 #include <g4detectors/PHG4CylinderSubsystem.h>
 
@@ -10,7 +10,6 @@
 #include <g4main/PHG4Reco.h>
 
 #include <fun4all/Fun4AllServer.h>
-
 
 #include <cmath>
 #include <vector>
@@ -37,29 +36,26 @@ namespace G4TPC
   double tpc_layer_thick_outer = 1.125;  // outer later reach from 60-78 cm (instead of 80 cm), that leads to radial thickness of 1.125 cm
   int tpc_layer_rphi_count_outer = 2304;
 
-
   double outer_radius = 78.;
   double inner_cage_radius = 20.;
   double cage_length = 211.0;  // From TPC group, gives eta = 1.1 at 78 cm
   double n_rad_length_cage = 1.13e-02;
   double cage_thickness = 28.6 * n_rad_length_cage;  // Kapton X_0 = 28.6 cm  // mocks up Kapton + carbon fiber structure
-// double inner_readout_radius = 30.; - deprecated for EIC simulation
 
-}
+  string tpcgas = "sPHENIX_TPC_Gas";  //  Ne(90%) CF4(10%) - defined in g4main/PHG4Reco.cc
+
+}  // namespace G4TPC
 
 void TPCInit(int verbosity = 0)
 {
-//  set at the end of the TPC function
-//  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, radius);
-
-  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, G4TPC::cage_length/2.);
-  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, -G4TPC::cage_length/2.);
-
+  //  BlackHoleGeometry::max_radius set at the end of the TPC function
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, G4TPC::cage_length / 2.);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, -G4TPC::cage_length / 2.);
 }
 
 double TPC(PHG4Reco* g4Reco, double radius,
-	   const int absorberactive = 0,
-	   int verbosity = 0)
+           const int absorberactive = 0,
+           int verbosity = 0)
 {
   // time projection chamber layers --------------------------------------------
 
@@ -79,27 +75,26 @@ double TPC(PHG4Reco* g4Reco, double radius,
 
   radius += G4TPC::cage_thickness;
 
-  // double inner_readout_radius = radius; 
+  // double inner_readout_radius = radius;
   //  if (inner_readout_radius < radius) inner_readout_radius = radius;
   //
-  string tpcgas = "sPHENIX_TPC_Gas";  //  Ne(90%) CF4(10%) - defined in g4main/PHG4Reco.cc
-                                      //
+  //
 
-  int n_tpc_layers[3] = {16,16,16};
-  int  tpc_layer_rphi_count[3] = {1152, 1536, 2304};
+  int n_tpc_layers[3] = {16, 16, 16};
+  int tpc_layer_rphi_count[3] = {1152, 1536, 2304};
 
-  double tpc_region_thickness[3] = { 20.,  20., 18.};
+  double tpc_region_thickness[3] = {20., 20., 18.};
   // Active layers of the TPC (inner layers)
   int nlayer = MVTX::n_maps_layer;
-  for (int irange=0; irange<3; irange++)
+  for (int irange = 0; irange < 3; irange++)
   {
-    double tpc_layer_thickness = tpc_region_thickness[irange]/n_tpc_layers[irange]; // thickness per layer
+    double tpc_layer_thickness = tpc_region_thickness[irange] / n_tpc_layers[irange];  // thickness per layer
     for (int ilayer = 0; ilayer < n_tpc_layers[irange]; ilayer++)
     {
       cyl = new PHG4CylinderSubsystem("SVTX", nlayer);
       cyl->set_double_param("radius", radius);
       cyl->set_double_param("length", G4TPC::cage_length);
-      cyl->set_string_param("material", tpcgas.c_str());
+      cyl->set_string_param("material", G4TPC::tpcgas);
       cyl->set_double_param("thickness", tpc_layer_thickness - 0.01);
       cyl->SetActive();
       cyl->SuperDetector("SVTX");
@@ -120,10 +115,9 @@ double TPC(PHG4Reco* g4Reco, double radius,
   cyl->SuperDetector("SVTXSUPPORT");
   g4Reco->registerSubsystem(cyl);
 
-  radius +=  G4TPC::cage_thickness;
+  radius += G4TPC::cage_thickness;
 
-// update now that we know the outer radius
+  // update now that we know the outer radius
   BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, radius);
   return radius;
 }
-
