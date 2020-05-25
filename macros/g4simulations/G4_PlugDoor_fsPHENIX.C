d27450566482cb12522fecd3f633cfa902b6308b
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-25 11:25:36 -0400

    use namespaces

--- macros/g4simulations/G4_PlugDoor_fsPHENIX.C
+++ macros/g4simulations/G4_PlugDoor_fsPHENIX.C
@@ -7,25 +7,25 @@
 
 R__LOAD_LIBRARY(libg4detectors.so)
 
+namespace Enable
+{
+  static bool PLUGDOOR = false;
+  static bool PLUGDOOR_ABSORBER = false;
+}
+
+namespace G4PLUGDOOR
+{
 const double place_z = 335.9;
 const double length = 10.2;
 const double inner_radius = 2.1;
 const double thickness = 258.5;
+}
 
 void PlugDoorInit()
 {
-  if (BlackHoleGeometry::max_radius < (inner_radius+thickness))
-  {
-    BlackHoleGeometry::max_radius = inner_radius+thickness;
-  }
-  if (BlackHoleGeometry::max_z < (place_z + length/2.))
-  {
-    BlackHoleGeometry::max_z = place_z + length/2.;
-  }
-  if (BlackHoleGeometry::min_z > -(place_z + length/2.))
-  {
-    BlackHoleGeometry::min_z = -(place_z + length/2.);
-  }
+  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius,G4PLUGDOOR::inner_radius+G4PLUGDOOR::thickness);
+    BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z,G4PLUGDOOR::place_z + G4PLUGDOOR::length/2.);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z,-(G4PLUGDOOR::place_z + G4PLUGDOOR::length/2.));
 }
 
 void PlugDoor(PHG4Reco *g4Reco,
@@ -35,24 +35,24 @@ void PlugDoor(PHG4Reco *g4Reco,
   //----------------------------------------
   // fsPHENIX forward flux return(s)
   const string material("Steel_1006");
-  const int flux_door_active = false;
+  const bool flux_door_active = Enable::ABSORBER || Enable::PLUGDOOR_ABSORBER ;
 
   PHG4CylinderSubsystem *flux_return_plus = new PHG4CylinderSubsystem("FWDFLUXRET", 0);
-  flux_return_plus->set_double_param("length", length);
-  flux_return_plus->set_double_param("radius", inner_radius);
-  flux_return_plus->set_double_param("place_z", place_z);
-  flux_return_plus->set_double_param("thickness", thickness);
+  flux_return_plus->set_double_param("length", G4PLUGDOOR::length);
+  flux_return_plus->set_double_param("radius", G4PLUGDOOR::inner_radius);
+  flux_return_plus->set_double_param("place_z", G4PLUGDOOR::place_z);
+  flux_return_plus->set_double_param("thickness", G4PLUGDOOR::thickness);
   flux_return_plus->set_string_param("material", material);
   flux_return_plus->SetActive(flux_door_active);
   flux_return_plus->SuperDetector("FLUXRET_ETA_PLUS");
   flux_return_plus->OverlapCheck(overlapcheck);
   g4Reco->registerSubsystem(flux_return_plus);
 
-  PHG4CylinderSubsystem *flux_return_minus = new PHG4CylinderSubsystem("FWDFLUXRET", 1);
-  flux_return_minus->set_double_param("length", length);
-  flux_return_minus->set_double_param("radius", inner_radius);
-  flux_return_minus->set_double_param("place_z", -place_z);
-  flux_return_minus->set_double_param("thickness", thickness);
+  PHG4CylinderSubsystem *flux_return_minus = new PHG4CylinderSubsystem("FWDFLUXRET", 0);
+  flux_return_minus->set_double_param("length", G4PLUGDOOR::length);
+  flux_return_minus->set_double_param("radius", G4PLUGDOOR::inner_radius);
+  flux_return_minus->set_double_param("place_z", -G4PLUGDOOR::place_z);
+  flux_return_minus->set_double_param("thickness", G4PLUGDOOR::thickness);
   flux_return_minus->set_string_param("material", material);
   flux_return_minus->SetActive(flux_door_active);
   flux_return_minus->SuperDetector("FLUXRET_ETA_MINUS");
