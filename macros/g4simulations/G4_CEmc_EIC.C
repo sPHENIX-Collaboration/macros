f8d268b5297c27ed8514f7088218e7a4cc5e0982
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-28 20:31:59 -0400

    Add comment about black hole setting

--- macros/g4simulations/G4_CEmc_EIC.C
+++ macros/g4simulations/G4_CEmc_EIC.C
@@ -66,6 +66,7 @@ namespace G4CEMC
   // enu_Cemc_clusterizer Cemc_clusterizer = kCemcGraphClusterizer;
 }  // namespace G4CEMC
 
+// Black hole and size parameters set in CEmc function
 void CEmcInit(const int nslats = 1)
 {
 }
