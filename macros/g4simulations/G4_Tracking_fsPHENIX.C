30d0b1f613dd12e552c47c2bb804023e1f29f2ef
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-29 19:59:03 -0400

    cleanup intt

--- macros/g4simulations/G4_Tracking_fsPHENIX.C
+++ macros/g4simulations/G4_Tracking_fsPHENIX.C
@@ -9,12 +9,6 @@
 
 #include <g4eval/SvtxEvaluator.h>
 
-#include <g4intt/PHG4InttDefs.h>
-#include <g4intt/PHG4InttDigitizer.h>
-#include <g4intt/PHG4InttSubsystem.h>
-#include <g4intt/PHG4InttHitReco.h>
-#include <g4intt/PHG4InttDefs.h>
-
 #include <g4main/PHG4Reco.h>
 
 #include <g4tpc/PHG4TpcDigitizer.h>
@@ -23,7 +17,6 @@
 #include <g4tpc/PHG4TpcPadPlaneReadout.h>
 #include <g4tpc/PHG4TpcSubsystem.h>
 
-#include <intt/InttClusterizer.h>
 #include <tpc/TpcClusterizer.h>
 
 #include <trackreco/PHGenFitTrkFitter.h>
@@ -38,33 +31,13 @@
 #include <trackbase/TrkrHitTruthAssoc.h>
 
 R__LOAD_LIBRARY(libg4tpc.so)
-R__LOAD_LIBRARY(libg4intt.so)
 R__LOAD_LIBRARY(libg4eval.so)
-R__LOAD_LIBRARY(libintt.so)
 R__LOAD_LIBRARY(libtpc.so)
 R__LOAD_LIBRARY(libtrack_reco.so)
 
 // Tracking simulation setup parameters and flag - leave them alone!
 //==============================================
 
-/////////////// INTT
-/*
-int n_intt_layer = 4;  // must be 4 or 0, setting to zero removes INTT completely
-int laddertype[4] = {PHG4InttDefs::SEGMENTATION_PHI,
-		       PHG4InttDefs::SEGMENTATION_PHI,
-		       PHG4InttDefs::SEGMENTATION_PHI,
-		       PHG4InttDefs::SEGMENTATION_PHI};
-int nladder[4] = {15,  15, 18, 18};
-double sensor_radius[4] = { 8.987, 9.545, 10.835, 11.361};  // radius of center of sensor for layer default
-double offsetphi[4] = {0.0, 0.5 * 360.0 / nladder[1] , 0.0, 0.5 * 360.0 / nladder[3]};
-enum enu_InttDeadMapType      // Dead map options for INTT
-{
-  kInttNoDeadMap = 0,        // All channel in Intt is alive
-  kIntt4PercentDeadMap = 4,  // 4% of dead/masked area (2% sensor + 2% chip) as a typical FVTX Run14 production run.
-  kIntt8PercentDeadMap = 8   // 8% dead/masked area (6% sensor + 2% chip) as threshold of operational
-};
-enu_InttDeadMapType InttDeadMapOption = kInttNoDeadMap;  // Choose Intt deadmap here
-*/
 ///////////////// TPC
 int n_tpc_layer_inner = 16;
 int tpc_layer_rphi_count_inner = 1152;
@@ -96,57 +69,6 @@ double Tracking(PHG4Reco* g4Reco, double radius,
                 const int absorberactive = 0,
                 int verbosity = 0)
 {
-  // create the three tracker subsystems
-/*
-  if (n_intt_layer > 0)
-  {
-    //-------------------
-    // INTT ladders
-    //-------------------
-
-    bool intt_overlapcheck = false;  // set to true if you want to check for overlaps
-
-    // instantiate the INTT subsystem and register it
-    // We make one instance of PHG4INTTSubsystem for all four layers of tracker
-    // dimensions are in mm, angles are in radians
-
-    // PHG4InttSubsystem creates the detetor layer using PHG4InttDetector
-    // and instantiates the appropriate PHG4SteppingAction
-    const double intt_radius_max = 140.;  // including stagger radius (mm)
-
-    // The length of vpair is used to determine the number of layers
-    std::vector<std::pair<int, int>> vpair;  // (sphxlayer, inttlayer)
-    for (int i = 0; i < n_intt_layer; i++)
-    {
-      // We want the sPHENIX layer numbers for the Intt to be from n_maps_layer to n_maps_layer+n_intt_layer - 1
-      vpair.push_back(std::make_pair(G4MVTX::n_maps_layer + i, i));  // sphxlayer=n_maps_layer+i corresponding to inttlayer=i
-      if (verbosity) cout << "Create strip tracker layer " << vpair[i].second << " as  sphenix layer  " << vpair[i].first << endl;
-    }
-
-    PHG4InttSubsystem* sitrack = new PHG4InttSubsystem("INTT", vpair);
-    sitrack->Verbosity(verbosity);
-    sitrack->SetActive(1);
-    sitrack->OverlapCheck(intt_overlapcheck);
-    g4Reco->registerSubsystem(sitrack);
-
-    // Set the laddertype and ladder spacing configuration
-
-    cout << "Intt has " << n_intt_layer << " layers with layer setup:" << endl;
-    for(int i=0;i<n_intt_layer;i++)
-      {
-	cout << " Intt layer " << i << " laddertype " << laddertype[i] << " nladders " << nladder[i]
-	     << " sensor radius " << sensor_radius[i] << " offsetphi " << offsetphi[i] << endl;
-	sitrack->set_int_param(i, "laddertype", laddertype[i]);
-	sitrack->set_int_param(i, "nladder", nladder[i]);
-	sitrack->set_double_param(i,"sensor_radius", sensor_radius[i]);  // expecting cm
-	sitrack->set_double_param(i,"offsetphi",offsetphi[i]);  // expecting degrees
-      }
-
-    // outer radius marker (translation back to cm)
-    radius = intt_radius_max * 0.1;
-  }
-*/
-
   // The Tpc - always present!
   //================================
   gSystem->Load("libg4tpc.so");
@@ -183,20 +105,6 @@ void Tracking_Cells(int verbosity = 0)
 
   Fun4AllServer* se = Fun4AllServer::instance();
 
-  // Intt hit reco
-  //===========
-/*
-  if (n_intt_layer > 0)
-  {
-    // new storage containers
-    PHG4InttHitReco* reco = new PHG4InttHitReco();
-    // The timing windows are hard-coded in the INTT ladder model, they can be overridden here
-    //reco->set_double_param("tmax",80.0);
-    //reco->set_double_param("tmin",-20.0);
-    reco->Verbosity(verbosity);
-    se->registerSubsystem(reco);
-  }
-*/
   //=========================
   // setup Tpc readout for filling cells
   // g4tpc/PHG4TpcElectronDrift uses
@@ -236,57 +144,6 @@ void Tracking_Clus(int verbosity = 0)
 
   Fun4AllServer* se = Fun4AllServer::instance();
 
-  //-------------------------------------------
-  // Digitize the hit energy into ADC
-  //------------------------------------------
-  if (G4INTT::n_intt_layer > 0)
-    {
-
-
-      // Intt
-      //=====
-      // these should be used for the Intt
-      /*
-	How threshold are calculated based on default FPHX settings
-	Four part information goes to the threshold calculation:
-	1. In 320 um thick silicon, the MIP e-h pair for a nominally indenting tracking is 3.87 MeV/cm * 320 um / 3.62 eV/e-h = 3.4e4 e-h pairs
-	2. From DOI: 10.1016/j.nima.2014.04.017, FPHX integrator amplifier gain is 100mV / fC. That translate MIP voltage to 550 mV.
-	3. From [FPHX Final Design Document](https://www.phenix.bnl.gov/WWW/fvtx/DetectorHardware/FPHX/FPHX2_June2009Revision.doc), the DAC0-7 setting for 8-ADC thresholds above the V_ref, as in Table 2 - Register Addresses and Defaults
-	4, From [FPHX Final Design Document](https://www.phenix.bnl.gov/WWW/fvtx/DetectorHardware/FPHX/FPHX2_June2009Revision.doc) section Front-end Program Bits, the formula to translate DAC setting to comparitor voltages.
-	The result threshold table based on FPHX default value is as following
-	| FPHX Register Address | Name            | Default value | Voltage - Vref (mV) | To electrons based on calibration | Electrons | Fraction to MIP |
-	|-----------------------|-----------------|---------------|---------------------|-----------------------------------|-----------|-----------------|
-	| 4                     | Threshold DAC 0 | 8             | 32                  | 2500                              | 2000      | 5.85E-02        |
-	| 5                     | Threshold DAC 1 | 16            | 64                  | 5000                              | 4000      | 1.17E-01        |
-	| 6                     | Threshold DAC 2 | 32            | 128                 | 10000                             | 8000      | 2.34E-01        |
-	| 7                     | Threshold DAC 3 | 48            | 192                 | 15000                             | 12000     | 3.51E-01        |
-	| 8                     | Threshold DAC 4 | 80            | 320                 | 25000                             | 20000     | 5.85E-01        |
-	| 9                     | Threshold DAC 5 | 112           | 448                 | 35000                             | 28000     | 8.18E-01        |
-	| 10                    | Threshold DAC 6 | 144           | 576                 | 45000                             | 36000     | 1.05E+00        |
-	| 11                    | Threshold DAC 7 | 176           | 704                 | 55000                             | 44000     | 1.29E+00        |
-	DAC0-7 threshold as fraction to MIP voltage are set to PHG4InttDigitizer::set_adc_scale as 3-bit ADC threshold as fractions to MIP energy deposition.
-      */
-/*
-      std::vector<double> userrange;  // 3-bit ADC threshold relative to the mip_e at each layer.
-      userrange.push_back(0.0584625322997416);
-      userrange.push_back(0.116925064599483);
-      userrange.push_back(0.233850129198966);
-      userrange.push_back(0.35077519379845);
-      userrange.push_back(0.584625322997416);
-      userrange.push_back(0.818475452196383);
-      userrange.push_back(1.05232558139535);
-      userrange.push_back(1.28617571059432);
-
-      // new containers
-      PHG4InttDigitizer* digiintt = new PHG4InttDigitizer();
-      digiintt->Verbosity(verbosity);
-      for (int i = 0; i < G4INTT::n_intt_layer; i++)
-	{
-	  digiintt->set_adc_scale(G4MVTX::n_maps_layer + i, userrange);
-	}
-      se->registerSubsystem(digiintt);
-*/
-    }
   // Tpc
   //====
   PHG4TpcDigitizer* digitpc = new PHG4TpcDigitizer();
@@ -305,20 +162,6 @@ void Tracking_Clus(int verbosity = 0)
   // Cluster Hits
   //-------------
 
-  // For the Intt layers
-  //===============
-/*
-  InttClusterizer* inttclusterizer = new InttClusterizer("InttClusterizer", G4MVTX::n_maps_layer, G4MVTX::n_maps_layer + n_intt_layer - 1);
-  inttclusterizer->Verbosity(verbosity);
-  // no Z clustering for Intt type 1 layers (we DO want Z clustering for type 0 layers)
-  // turning off phi clustering for type 0 layers is not necessary, there is only one strip per sensor in phi
-  for (int i = G4MVTX::n_maps_layer; i < G4MVTX::n_maps_layer + n_intt_layer; i++)
-  {
-    if (laddertype[i - G4MVTX::n_maps_layer] == PHG4InttDefs::SEGMENTATION_PHI)
-      inttclusterizer->set_z_clustering(i, false);
-  }
-  se->registerSubsystem(inttclusterizer);
-*/
   // For the Tpc
   //==========
   TpcClusterizer* tpcclusterizer = new TpcClusterizer();
