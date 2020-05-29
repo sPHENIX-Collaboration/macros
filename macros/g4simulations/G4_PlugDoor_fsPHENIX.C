c9542db0ca63d31dd44886ec08437c4bfd2a8672
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-29 11:06:48 -0400

    Add Enable driven OVERLAP check

--- macros/g4simulations/G4_PlugDoor_fsPHENIX.C
+++ macros/g4simulations/G4_PlugDoor_fsPHENIX.C
@@ -3,6 +3,7 @@
 #include "GlobalVariables.C"
 
 #include <g4detectors/PHG4CylinderSubsystem.h>
+
 #include <g4main/PHG4Reco.h>
 
 R__LOAD_LIBRARY(libg4detectors.so)
@@ -11,6 +12,7 @@ namespace Enable
 {
   static bool PLUGDOOR = false;
   static bool PLUGDOOR_ABSORBER = false;
+  static bool PLUGDOOR_OVERLAPCHECK = false;
 }  // namespace Enable
 
 namespace G4PLUGDOOR
@@ -30,10 +32,14 @@ void PlugDoorInit()
 
 void PlugDoor(PHG4Reco *g4Reco)
 {
+
+
   //----------------------------------------
   // fsPHENIX forward flux return(s)
+  bool OverlapCheck = Enable::OVERLAPCHECK || Enable::PLUGDOOR_OVERLAPCHECK;
+  bool flux_door_active = Enable::ABSORBER || Enable::PLUGDOOR_ABSORBER;
+
   const string material("Steel_1006");
-  const bool flux_door_active = Enable::ABSORBER || Enable::PLUGDOOR_ABSORBER;
 
   PHG4CylinderSubsystem *flux_return_plus = new PHG4CylinderSubsystem("FWDFLUXRET", 0);
   flux_return_plus->set_double_param("length", G4PLUGDOOR::length);
@@ -43,7 +49,7 @@ void PlugDoor(PHG4Reco *g4Reco)
   flux_return_plus->set_string_param("material", material);
   flux_return_plus->SetActive(flux_door_active);
   flux_return_plus->SuperDetector("FLUXRET_ETA_PLUS");
-  flux_return_plus->OverlapCheck(overlapcheck);
+  flux_return_plus->OverlapCheck(OverlapCheck);
   g4Reco->registerSubsystem(flux_return_plus);
 
   PHG4CylinderSubsystem *flux_return_minus = new PHG4CylinderSubsystem("FWDFLUXRET", 0);
@@ -54,7 +60,7 @@ void PlugDoor(PHG4Reco *g4Reco)
   flux_return_minus->set_string_param("material", material);
   flux_return_minus->SetActive(flux_door_active);
   flux_return_minus->SuperDetector("FLUXRET_ETA_MINUS");
-  flux_return_minus->OverlapCheck(overlapcheck);
+  flux_return_minus->OverlapCheck(OverlapCheck);
   g4Reco->registerSubsystem(flux_return_minus);
 
   return;
