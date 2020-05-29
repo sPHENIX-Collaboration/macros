d5d1d91622242852376d351f70a25ce500509492
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-29 10:59:48 -0400

    apply black hole geometry

--- macros/g4simulations/G4_Piston.C
+++ macros/g4simulations/G4_Piston.C
@@ -21,12 +21,15 @@ namespace Enable
 
 namespace G4PISTON
 {
-}  // namespace G4PIPE
+  static double zpos1 = 305. - 20.; // front of forward ECal/MPC
+  static double zpos2 = 335.9 - 10.2 / 2.; // front of the forward field endcap
+  static double calorimeter_hole_diameter = 9.92331 *2; // side length of the middle hole of MPC that
+}  // namespace G4PISTON
 
 void PistonInit()
 {
-  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, 50.);
-  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z,305. - 20. );
+  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius,  G4PISTON::calorimeter_hole_diameter/2.);
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z,((G4PISTON::zpos2 + G4PISTON::zpos1) / 2.+(G4PISTON::zpos2 - G4PISTON::zpos1)/2.) );
 }
 
 void Piston(PHG4Reco* g4Reco,
@@ -38,9 +41,6 @@ void Piston(PHG4Reco* g4Reco,
 
   string name = "magpiston";
   const double zpos0 = G4PIPE::al_pipe_length + G4PIPE::be_pipe_length * 0.5; // first large GEM station
-  const double zpos1 = 305 - 20; // front of forward ECal/MPC
-  const double zpos2 = 335.9 - 10.2 / 2.; // front of the forward field endcap
-  const double calorimeter_hole_diamater = 9.92331 *2; // side length of the middle hole of MPC that can hold the piston. Also the max diameter of the piston in that region
 
   const double beampipe_radius = G4PIPE::be_pipe_radius;
 
@@ -50,23 +50,23 @@ void Piston(PHG4Reco* g4Reco,
   const double eta_inner = -log(tan(atan((beampipe_radius + 0.1) / zpos0) / 2));
   const double eta_outter = 4.2;
   const double eta_teeth_outter = 4.05;
-  double pos = zpos0 + (zpos1 - zpos0) / 2;
+  double pos = zpos0 + (G4PISTON::zpos1 - zpos0) / 2;
 //  cout << "MAGNETIC PISTON:" << eta_inner << " " << eta_outter << " " << pos
 //      << endl;
 
   PHG4ConeSubsystem *magpiston = new PHG4ConeSubsystem("Piston", 0);
-  magpiston->SetZlength((zpos1 - zpos0) / 2);
-  magpiston->SetPlaceZ((zpos1 + zpos0) / 2);
+  magpiston->SetZlength((G4PISTON::zpos1 - zpos0) / 2);
+  magpiston->SetPlaceZ((G4PISTON::zpos1 + zpos0) / 2);
   magpiston->SetR1(beampipe_radius,
       tan(PHG4Sector::Sector_Geometry::eta_to_polar_angle(eta_outter)) * zpos0);
   magpiston->SetR2(beampipe_radius,
-      tan(PHG4Sector::Sector_Geometry::eta_to_polar_angle(eta_outter)) * zpos1);
+      tan(PHG4Sector::Sector_Geometry::eta_to_polar_angle(eta_outter)) * G4PISTON::zpos1);
   magpiston->SetMaterial("G4_Fe");
   magpiston->OverlapCheck(OverlapCheck);
   g4Reco->registerSubsystem(magpiston);
 
 //  PHG4ConeSubsystem *magpiston = new PHG4ConeSubsystem(name, 1);
-//  magpiston->SetZlength((zpos1 - zpos0) / 2);
+//  magpiston->SetZlength((G4PISTON::zpos1 - zpos0) / 2);
 //  magpiston->SetPlaceZ(pos);
 //  magpiston->Set_eta_range(eta_outter, eta_inner);
 //  magpiston->SetMaterial("G4_Fe");
@@ -105,15 +105,15 @@ void Piston(PHG4Reco* g4Reco,
       if (AbsorberActive) magpiston->SetActive();
       magpiston->OverlapCheck(OverlapCheck);
       g4Reco->registerSubsystem(magpiston);
-      pos += ((zpos1 - zpos0 - 10) / number_of_wteeth);
+      pos += ((G4PISTON::zpos1 - zpos0 - 10) / number_of_wteeth);
     }
 
   // last piece connect to the field return
   PHG4CylinderSubsystem *magpiston2 = new PHG4CylinderSubsystem("Piston_EndSection", 0);
-  magpiston2->set_double_param("length",zpos2 - zpos1);
-  magpiston2->set_double_param("place_z", (zpos2 + zpos1) / 2.);
+  magpiston2->set_double_param("length",G4PISTON::zpos2 - G4PISTON::zpos1);
+  magpiston2->set_double_param("place_z", (G4PISTON::zpos2 + G4PISTON::zpos1) / 2.);
   magpiston2->set_double_param("radius",beampipe_radius);
-  magpiston2->set_double_param("thickness",calorimeter_hole_diamater / 2. - beampipe_radius);
+  magpiston2->set_double_param("thickness",G4PISTON::calorimeter_hole_diameter / 2. - beampipe_radius);
   magpiston2->set_string_param("material","G4_Fe");
   magpiston2->SuperDetector(name);
   if (AbsorberActive) magpiston2->SetActive();
