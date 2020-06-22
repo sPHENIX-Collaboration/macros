b398a8076fb2c1ee9364752cafac655673dce2f9
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-06-22 19:18:16 -0400

    merge with main

--- macros/g4simulations/G4_Tracking_EIC.C
+++ macros/g4simulations/G4_Tracking_EIC.C
@@ -26,13 +26,27 @@ R__LOAD_LIBRARY(libg4trackfastsim.so)
 namespace Enable
 {
   bool TRACKING = false;
+  bool TRACKING_EVAL = false;
+  int TRACKING_VERBOSITY = 0;
+}  // namespace Enable
+
+namespace G4TRACKING
+{
+  bool DISPLACED_VERTEX = false;
+  bool PROJECTION_CEMC = false;
+  bool PROJECTION_FEMC = false;
+  bool PROJECTION_FHCAL = false;
+}  // namespace G4TRACKING
+
+//-----------------------------------------------------------------------------//
+void TrackingInit()
+{
+  TRACKING::TrackNodeName = "TrackMap";
 }
 //-----------------------------------------------------------------------------//
-void TrackingInit() {}
-//-----------------------------------------------------------------------------//
-void Tracking_Reco(int verbosity = 0, bool displaced_vertex = false)
+void Tracking_Reco()
 {
-  int i;
+  int verbosity = std::max(Enable::VERBOSITY, Enable::TRACKING_VERBOSITY);
   //---------------
   // Fun4All server
   //---------------
@@ -42,145 +56,151 @@ void Tracking_Reco(int verbosity = 0, bool displaced_vertex = false)
   PHG4TrackFastSim *kalman = new PHG4TrackFastSim("PHG4TrackFastSim");
   //  kalman->Verbosity();
   //  kalman->Smearing(false);
-  if (displaced_vertex)
-    {
-      //use very loose vertex constraint (1cm in sigma) to allow reco of displaced vertex
-      kalman->set_use_vertex_in_fitting(true);
-      kalman->set_vertex_xy_resolution(1);
-      kalman->set_vertex_z_resolution(1);
-      kalman->enable_vertexing(true);
-    }
+  if (G4TRACKING::DISPLACED_VERTEX)
+  {
+    //use very loose vertex constraint (1cm in sigma) to allow reco of displaced vertex
+    kalman->set_use_vertex_in_fitting(true);
+    kalman->set_vertex_xy_resolution(1);
+    kalman->set_vertex_z_resolution(1);
+    kalman->enable_vertexing(true);
+  }
   else
-    {
-      // constraint to a primary vertex and use it as part of the fitting level arm
-      kalman->set_use_vertex_in_fitting(true);
-      kalman->set_vertex_xy_resolution(50e-4);
-      kalman->set_vertex_z_resolution(50e-4);
-    }
+  {
+    // constraint to a primary vertex and use it as part of the fitting level arm
+    kalman->set_use_vertex_in_fitting(true);
+    kalman->set_vertex_xy_resolution(50e-4);
+    kalman->set_vertex_z_resolution(50e-4);
+  }
 
-  kalman->set_sub_top_node_name("SVTX");
-  kalman->set_trackmap_out_name("SvtxTrackMap");
+  kalman->set_sub_top_node_name("TRACKS");
+  kalman->set_trackmap_out_name(TRACKING::TrackNodeName);
 
   //-------------------------
   // Barrel
   //-------------------------
-  if (Enable::BARREL) {
+  if (Enable::BARREL)
+  {
     kalman->add_phg4hits("G4HIT_BARREL",              //      const std::string& phg4hitsNames,
-			 PHG4TrackFastSim::Cylinder,  //      const DETECTOR_TYPE phg4dettype,
-			 5e-4,                        //      const float radres,
-			 5e-4,                        //      const float phires,
-			 5e-4,                        //      const float lonres,
-			 1,                           //      const float eff,
-			 0);                          //      const float noise
+                         PHG4TrackFastSim::Cylinder,  //      const DETECTOR_TYPE phg4dettype,
+                         5e-4,                        //      const float radres,
+                         5e-4,                        //      const float phires,
+                         5e-4,                        //      const float lonres,
+                         1,                           //      const float eff,
+                         0);                          //      const float noise
   }
   //-------------------------
   // MVTX
   //-------------------------
   if (Enable::MVTX)
-    {
-      //   MAPS
-      kalman->add_phg4hits(
-			   "G4HIT_MVTX",                //      const std::string& phg4hitsNames,
-			   PHG4TrackFastSim::Cylinder,  //      const DETECTOR_TYPE phg4dettype,
-			   5e-4,                        //      const float radres,
-			   5e-4,                        //      const float phires,
-			   5e-4,                        //      const float lonres,
-			   1,                           //      const float eff,
-			   0                            //      const float noise
-			   );
-    }
+  {
+    //   MAPS
+    kalman->add_phg4hits(
+        "G4HIT_MVTX",                //      const std::string& phg4hitsNames,
+        PHG4TrackFastSim::Cylinder,  //      const DETECTOR_TYPE phg4dettype,
+        5e-4,                        //      const float radres,
+        5e-4,                        //      const float phires,
+        5e-4,                        //      const float lonres,
+        1,                           //      const float eff,
+        0                            //      const float noise
+    );
+  }
   //-------------------------
   // TPC
   //-------------------------
   if (Enable::TPC)
-    {
-      kalman->add_phg4hits(
-			   "G4HIT_SVTX",                //      const std::string& phg4hitsNames,
-			   PHG4TrackFastSim::Cylinder,  //      const DETECTOR_TYPE phg4dettype,
-			   1,                           //      const float radres,
-			   200e-4,                      //      const float phires,
-			   500e-4,                      //      const float lonres,
-			   1,                           //      const float eff,
-			   0                            //      const float noise
-			   );
-    }
+  {
+    kalman->add_phg4hits(
+        "G4HIT_TPC",                 //      const std::string& phg4hitsNames,
+        PHG4TrackFastSim::Cylinder,  //      const DETECTOR_TYPE phg4dettype,
+        1,                           //      const float radres,
+        200e-4,                      //      const float phires,
+        500e-4,                      //      const float lonres,
+        1,                           //      const float eff,
+        0                            //      const float noise
+    );
+  }
   //-------------------------
   // EGEM
   //-------------------------
   if (Enable::EGEM)
+  {
+    // GEM, 70um azimuthal resolution, 1cm radial strips
+    for (int i = 0; i < 4; i++)
     {
-      // GEM, 70um azimuthal resolution, 1cm radial strips
-      for (i=0;i<4;i++) {
-	kalman->add_phg4hits(
-			     Form("G4HIT_EGEM_%d",i),           //      const std::string& phg4hitsNames,
-			     PHG4TrackFastSim::Vertical_Plane,  //      const DETECTOR_TYPE phg4dettype,
-			     1. / sqrt(12.),                    //      const float radres,
-			     70e-4,                             //      const float phires,
-			     100e-4,                            //      const float lonres,
-			     1,                                 //      const float eff,
-			     0                                  //      const float noise
-			     );
-      }
+      kalman->add_phg4hits(
+          Form("G4HIT_EGEM_%d", i),          //      const std::string& phg4hitsNames,
+          PHG4TrackFastSim::Vertical_Plane,  //      const DETECTOR_TYPE phg4dettype,
+          1. / sqrt(12.),                    //      const float radres,
+          70e-4,                             //      const float phires,
+          100e-4,                            //      const float lonres,
+          1,                                 //      const float eff,
+          0                                  //      const float noise
+      );
     }
+  }
   //-------------------------
   // FGEM
   //-------------------------
   if (Enable::FGEM)
+  {
+    // GEM2, 70um azimuthal resolution, 1cm radial strips
+    for (int i = 2; i < 5; i++)
     {
-      // GEM2, 70um azimuthal resolution, 1cm radial strips
-      for (i=2;i<5;i++) {
-	kalman->add_phg4hits(Form("G4HIT_FGEM_%d",i),           //      const std::string& phg4hitsNames,
-			     PHG4TrackFastSim::Vertical_Plane,  //      const DETECTOR_TYPE phg4dettype,
-			     1. / sqrt(12.),                    //      const float radres,
-			     70e-4,                             //      const float phires,
-			     100e-4,                            //      const float lonres,
-			     1,                                 //      const float eff,
-			     0);                                //      const float noise
-      }
+      kalman->add_phg4hits(Form("G4HIT_FGEM_%d", i),          //      const std::string& phg4hitsNames,
+                           PHG4TrackFastSim::Vertical_Plane,  //      const DETECTOR_TYPE phg4dettype,
+                           1. / sqrt(12.),                    //      const float radres,
+                           70e-4,                             //      const float phires,
+                           100e-4,                            //      const float lonres,
+                           1,                                 //      const float eff,
+                           0);                                //      const float noise
     }
+  }
   //-------------------------
   // FST
   //-------------------------
-  if (Enable::FST) {
-    for (i=0;i<5;i++) {
-      kalman->add_phg4hits(Form("G4HIT_FST_%d",i),            //      const std::string& phg4hitsNames,                                   
-			   PHG4TrackFastSim::Vertical_Plane,  //      const DETECTOR_TYPE phg4dettype,                                    
-			   5e-4,                              //      const float radres,                                                 
-			   5e-4,                              //      const float phires,                                                 
-			   50e-4 / sqrt(12.),                 //      const float lonres,                                                 
-			   1,                                 //      const float eff,                                                    
-			   0);                                //      const float noise                                                   
+  if (Enable::FST)
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      kalman->add_phg4hits(Form("G4HIT_FST_%d", i),           //      const std::string& phg4hitsNames,
+                           PHG4TrackFastSim::Vertical_Plane,  //      const DETECTOR_TYPE phg4dettype,
+                           5e-4,                              //      const float radres,
+                           5e-4,                              //      const float phires,
+                           50e-4 / sqrt(12.),                 //      const float lonres,
+                           1,                                 //      const float eff,
+                           0);                                //      const float noise
     }
-  } 
+  }
   //-------------------------
   // FEMC
   //-------------------------
   // Saved track states (projections)
   if (Enable::FEMC)
-    {
-      //    kalman->add_state_name("FEMC");
-    }
+  {
+    //    kalman->add_state_name("FEMC");
+  }
   //-------------------------
   // FHCAL
   //-------------------------
   if (Enable::FHCAL)
-    {
-      //    kalman->add_state_name("FHCAL");
-    }
+  {
+    //    kalman->add_state_name("FHCAL");
+  }
   //-------------------------
   // CEMC
   //-------------------------
   if (Enable::CEMC)
-    {
-      //    kalman->add_state_name("CEMC");
-    }
+  {
+    //    kalman->add_state_name("CEMC");
+  }
   se->registerSubsystem(kalman);
 
   return;
 }
 //-----------------------------------------------------------------------------//
-void Tracking_Eval(std::string outputfile, int verbosity = 0)
+void Tracking_Eval(const std::string &outputfile)
 {
+  int verbosity = std::max(Enable::VERBOSITY, Enable::TRACKING_VERBOSITY);
   //---------------
   // Fun4All server
   //---------------
@@ -192,6 +212,7 @@ void Tracking_Eval(std::string outputfile, int verbosity = 0)
   //----------------
 
   PHG4TrackFastSimEval *fast_sim_eval = new PHG4TrackFastSimEval("FastTrackingEval");
-  fast_sim_eval->set_filename(outputfile.c_str());
+  fast_sim_eval->set_trackmapname(TRACKING::TrackNodeName);
+  fast_sim_eval->set_filename(outputfile);
   se->registerSubsystem(fast_sim_eval);
 }
