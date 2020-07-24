de01fb98bf464debe597dfb644b44e2edd3bb97c
cdean-github <cameron.dean@cern.ch>
2020-07-24 14:40:00 -0400

    Added missing namespace
    Also added 3 variables that were out-of-scope for the service barrel. Simulation has been checked in local common area

--- common/G4_Mvtx.C
+++ common/G4_Mvtx.C
@@ -28,6 +28,7 @@ namespace Enable
   bool MVTX_OVERLAPCHECK = false;
   bool MVTX_CELL = false;
   bool MVTX_CLUSTER = false;
+  bool MVTX_ABSORBER = false;
   bool MVTX_SERVICE = true;
   int MVTX_VERBOSITY = 0;
 
@@ -60,66 +61,6 @@ void MvtxInit()
   BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, -17.);
 }
 
-double Mvtx(PHG4Reco* g4Reco, double radius,
-            const int absorberactive = 0)
-{
-  bool maps_overlapcheck = Enable::OVERLAPCHECK || Enable::MVTX_OVERLAPCHECK;
-  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
-
-  PHG4MvtxSubsystem* mvtx = new PHG4MvtxSubsystem("MVTX");
-  mvtx->Verbosity(verbosity);
-
-  for (int ilayer = 0; ilayer < G4MVTX::n_maps_layer; ilayer++)
-  {
-    double radius_lyr = PHG4MvtxDefs::mvtxdat[ilayer][PHG4MvtxDefs::kRmd];
-    if (verbosity)
-    {
-      cout << "Create Maps layer " << ilayer << " with radius " << radius_lyr << " mm." << endl;
-    }
-    radius = radius_lyr / 10.;
-  }
-  mvtx->set_string_param(PHG4MvtxDefs::GLOBAL, "stave_geometry_file", string(getenv("CALIBRATIONROOT")) + string("/Tracking/geometry/mvtx_stave_v1.gdml"));
-  mvtx->SetActive();
-  mvtx->OverlapCheck(maps_overlapcheck);
-  g4Reco->registerSubsystem(mvtx);
-  radius += G4MVTX::radius_offset;
-  if (MVTX_SERVICE) radius += MVTXService(g4Reco, radius);
-  return radius;
-}
-
-// Central detector cell reco is disabled as EIC setup use the fast tracking sim for now
-void Mvtx_Cells()
-{
-  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
-  Fun4AllServer* se = Fun4AllServer::instance();
-  // new storage containers
-  PHG4MvtxHitReco* maps_hits = new PHG4MvtxHitReco("MVTX");
-  maps_hits->Verbosity(verbosity);
-  for (int ilayer = 0; ilayer < G4MVTX::n_maps_layer; ilayer++)
-  {
-    // override the default timing window for this layer - default is +/- 5000 ns
-    maps_hits->set_timing_window(ilayer, -5000, 5000);
-  }
-  se->registerSubsystem(maps_hits);
-  return;
-}
-
-void Mvtx_Clustering()
-{
-  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
-  Fun4AllServer* se = Fun4AllServer::instance();
-  PHG4MvtxDigitizer* digimvtx = new PHG4MvtxDigitizer();
-  digimvtx->Verbosity(verbosity);
-  // energy deposit in 25 microns = 9.6 KeV = 1000 electrons collected after recombination
-  //digimvtx->set_adc_scale(0.95e-6);  // default set in code is 0.95e-06, which is 99 electrons
-  se->registerSubsystem(digimvtx);
-  // For the Mvtx layers
-  //================
-  MvtxClusterizer* mvtxclusterizer = new MvtxClusterizer("MvtxClusterizer");
-  mvtxclusterizer->Verbosity(verbosity);
-  se->registerSubsystem(mvtxclusterizer);
-}
-
 double calculateArea( double inner_radius, double outer_radius ) //Calculate the area of a disk
 {
 	return pi*( std::pow( outer_radius, 2 ) - std::pow( inner_radius, 2) );
@@ -141,6 +82,10 @@ double MVTXService(PHG4Reco* g4Reco, double radius)
 {
 // Note, cables are all south  
 // Setup service_layers
+  bool AbsorberActive = Enable::ABSORBER || Enable::MVTX_ABSORBER;
+  bool OverlapCheck = Enable::OVERLAPCHECK || Enable::MVTX_OVERLAPCHECK;
+  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
+
   double copper_OR[G4MVTX::n_service_layers], water_OR[G4MVTX::n_service_layers], plastic_OR[G4MVTX::n_service_layers]; //Objects for material outer radii
   int subsystem_service_layer = 0;
   std::string copper_name, water_name, plastic_name;
@@ -234,5 +179,66 @@ double MVTXService(PHG4Reco* g4Reco, double radius)
 
   return radius;
 }
+
+double Mvtx(PHG4Reco* g4Reco, double radius,
+            const int absorberactive = 0)
+{
+  bool maps_overlapcheck = Enable::OVERLAPCHECK || Enable::MVTX_OVERLAPCHECK;
+  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
+
+  PHG4MvtxSubsystem* mvtx = new PHG4MvtxSubsystem("MVTX");
+  mvtx->Verbosity(verbosity);
+
+  for (int ilayer = 0; ilayer < G4MVTX::n_maps_layer; ilayer++)
+  {
+    double radius_lyr = PHG4MvtxDefs::mvtxdat[ilayer][PHG4MvtxDefs::kRmd];
+    if (verbosity)
+    {
+      cout << "Create Maps layer " << ilayer << " with radius " << radius_lyr << " mm." << endl;
+    }
+    radius = radius_lyr / 10.;
+  }
+  mvtx->set_string_param(PHG4MvtxDefs::GLOBAL, "stave_geometry_file", string(getenv("CALIBRATIONROOT")) + string("/Tracking/geometry/mvtx_stave_v1.gdml"));
+  mvtx->SetActive();
+  mvtx->OverlapCheck(maps_overlapcheck);
+  g4Reco->registerSubsystem(mvtx);
+  radius += G4MVTX::radius_offset;
+  if (Enable::MVTX_SERVICE) radius += MVTXService(g4Reco, radius);
+  return radius;
+}
+
+// Central detector cell reco is disabled as EIC setup use the fast tracking sim for now
+void Mvtx_Cells()
+{
+  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
+  Fun4AllServer* se = Fun4AllServer::instance();
+  // new storage containers
+  PHG4MvtxHitReco* maps_hits = new PHG4MvtxHitReco("MVTX");
+  maps_hits->Verbosity(verbosity);
+  for (int ilayer = 0; ilayer < G4MVTX::n_maps_layer; ilayer++)
+  {
+    // override the default timing window for this layer - default is +/- 5000 ns
+    maps_hits->set_timing_window(ilayer, -5000, 5000);
+  }
+  se->registerSubsystem(maps_hits);
+  return;
+}
+
+void Mvtx_Clustering()
+{
+  int verbosity = std::max(Enable::VERBOSITY, Enable::MVTX_VERBOSITY);
+  Fun4AllServer* se = Fun4AllServer::instance();
+  PHG4MvtxDigitizer* digimvtx = new PHG4MvtxDigitizer();
+  digimvtx->Verbosity(verbosity);
+  // energy deposit in 25 microns = 9.6 KeV = 1000 electrons collected after recombination
+  //digimvtx->set_adc_scale(0.95e-6);  // default set in code is 0.95e-06, which is 99 electrons
+  se->registerSubsystem(digimvtx);
+  // For the Mvtx layers
+  //================
+  MvtxClusterizer* mvtxclusterizer = new MvtxClusterizer("MvtxClusterizer");
+  mvtxclusterizer->Verbosity(verbosity);
+  se->registerSubsystem(mvtxclusterizer);
+}
+
 
 #endif
