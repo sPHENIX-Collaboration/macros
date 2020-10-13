f46dd759857fd87f926ba48ecf3394c074324deb
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-10-13 17:01:30 -0400

    use hits fril from webserver

--- detectors/sPHENIX/Fun4All_G4_sPHENIX.C
+++ detectors/sPHENIX/Fun4All_G4_sPHENIX.C
@@ -26,9 +26,12 @@
 
 R__LOAD_LIBRARY(libfun4all.so)
 
+// For HepMC Hijing
+// try inputFile = /sphenix/sim/sim01/sphnxpro/sHijing_HepMC/sHijing_0-12fm.dat
+
 int Fun4All_G4_sPHENIX(
     const int nEvents = 1,
-    const string &inputFile = "/sphenix/data/data02/review_2017-08-02/single_particle/spacal2d/fieldmap/G4Hits_sPHENIX_e-_eta0_8GeV-0002.root",
+    const string &inputFile = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
     const string &outputFile = "G4sPHENIX.root",
     const string &embed_input_file = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
     const int skip = 0,
