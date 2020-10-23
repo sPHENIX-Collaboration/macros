a8d199d0b648094d6577394de9e1bff4cdb5800b
mchiu-bnl <chiu@bnl.gov>
2020-10-23 11:47:26 -0400

    Added MBD/BBC

--- common/G4_Bbc.C
+++ common/G4_Bbc.C
@@ -1,9 +1,9 @@
-#ifndef MACRO_G4BBC_C
-#define MACRO_G4BBC_C
+#ifndef COMMON_G4BBC_C
+#define COMMON_G4BBC_C
 
 #include <g4bbc/BbcVertexFastSimReco.h>
 #include <fun4all/Fun4AllServer.h>
-#include <g4detectors/PHG4BbcSubsystem.h>
+#include "g4detectors/PHG4BbcSubsystem.h"
 
 
 R__LOAD_LIBRARY(libg4bbc.so)
@@ -11,6 +11,7 @@ R__LOAD_LIBRARY(libg4bbc.so)
 namespace Enable
 {
   bool BBC = false;
+  bool BBCFAKE = false;
 }
 
 namespace G4BBC
@@ -21,9 +22,30 @@ namespace G4BBC
 
 void BbcInit()
 {
-  cout << "In BbcInit()" << endl;
-  // Nothing to Init for now
+  //cout << "In BbcInit()" << endl;
+
+  // Set boundary of tracked particles to include BBC (values in cm)
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, 300.);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, -300.);
+  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, 15.0);
+
 }
+
+
+double Bbc(PHG4Reco* g4Reco, double radius)
+{
+  //cout << "XXXX CREATING BBC" << endl;
+
+  gSystem->Load("libg4detectors.so");
+
+  PHG4BbcSubsystem *bbc = new PHG4BbcSubsystem( "BBC" );
+  bbc->SuperDetector( "BBC" );
+  g4Reco->registerSubsystem( bbc );
+
+  radius = 15.;  // outer shell is 15 cm
+  return radius; 
+}
+
 
 void Bbc_Reco(int verbosity = 0)
 {
@@ -33,9 +55,8 @@ void Bbc_Reco(int verbosity = 0)
 
   Fun4AllServer* se = Fun4AllServer::instance();
 
-  // disable fast sim for now
-  int do_bbcfast = 0;
-  if ( do_bbcfast )
+  //if ( Enable::BBCFAKE == true )
+  if ( Enable::BBC == true )
   {
     BbcVertexFastSimReco* bbcvertex = new BbcVertexFastSimReco();
     bbcvertex->set_z_smearing(G4BBC::z_smearing);  // 6 mm, temporarily perfect for TPC initial vertexing
