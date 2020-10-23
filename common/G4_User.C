de066e66e0e50ef0ab3a7574a9b4cdfc3bcbe32f
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-10-23 13:07:47 -0400

    Add UserAnalysisInit() function for user modules

--- common/G4_User.C
+++ common/G4_User.C
@@ -3,15 +3,23 @@
 
 namespace Enable
 {
+// if you want this to run by default, initialize this to true
+// Otherwise you have to use Enable::USER = true; in your macro
   bool USER = false;
+  int USER_VERBOSITY = 0;
 }
 
 namespace G4USER
 {
+// here you can set parameters in your macro via
+// G4USER::myparam = 1;
+// add as many as you need
   int myparam = 0;
 }
 
-void UserInit(int verbosity = 0)
+// This initializes the G4 part if you have a detector implemented
+// You need to tell its dimensions to the surrounding black hole
+void UserInit()
 {
   // set the black hole dimensions surrounding the detector
   // XXX: maximum radius of your detector
@@ -22,8 +30,16 @@ void UserInit(int verbosity = 0)
   //BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, ZZZ);
 }
 
+// If you have a detector - here goes the setup
 void UserDetector(PHG4Reco *g4Reco)
 {
   return;
 }
+
+// Any analysis goes here (registering your module with Fun4All)
+void UserAnalysisInit()
+{
+  return;
+}
+
 #endif
