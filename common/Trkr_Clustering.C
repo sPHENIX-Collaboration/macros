ca72318ecdc2e1c6a92962299b4ac29b6894c65e
bkimelman <benjamin.kimelman@vanderbilt.edu>
2023-04-14 11:01:15 -0400

    Moved TPC distortion calls to Trkr_Clustering due to dependencies and removed Trkr_LaserClustering as no longer necessary

--- common/Trkr_Clustering.C
+++ common/Trkr_Clustering.C
@@ -13,9 +13,14 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wundefined-internal"
 #include <tpc/TpcClusterizer.h>
+#include <tpc/TpcSimpleClusterizer.h>
 #pragma GCC diagnostic pop
 
 #include <tpc/TpcClusterCleaner.h>
+
+#include <tpccalib/PHTpcCentralMembraneClusterizer.h>
+#include <tpccalib/PHTpcCentralMembraneMatcher.h>
+#include <tpccalib/TpcDirectLaserReconstruction.h>
 
 #include <micromegas/MicromegasClusterizer.h>
 
@@ -84,19 +89,60 @@ void TPC_Clustering()
 
   // For the Tpc
   //==========
-
-  auto tpcclusterizer = new TpcClusterizer;
-  tpcclusterizer->Verbosity(verbosity);
-  tpcclusterizer->set_cluster_version(G4TRACKING::cluster_version);
-  tpcclusterizer->set_do_hit_association( G4TPC::DO_HIT_ASSOCIATION );
-  se->registerSubsystem(tpcclusterizer);
-  
+  if( G4TPC::USE_SIMPLE_CLUSTERIZER )
+    {
+      auto tpcclusterizer = new TpcSimpleClusterizer;
+      tpcclusterizer->Verbosity(verbosity);
+      se->registerSubsystem(tpcclusterizer);
+      
+    }
+  else
+    {
+      auto tpcclusterizer = new TpcClusterizer;
+      tpcclusterizer->Verbosity(verbosity);
+      tpcclusterizer->set_cluster_version(G4TRACKING::cluster_version);
+      tpcclusterizer->set_do_hit_association( G4TPC::DO_HIT_ASSOCIATION );
+      se->registerSubsystem(tpcclusterizer);
+    }
  
-  auto tpcclustercleaner = new TpcClusterCleaner;
-  tpcclustercleaner->Verbosity(verbosity);
-  tpcclustercleaner->set_cluster_version(G4TRACKING::cluster_version);
-  se->registerSubsystem(tpcclustercleaner);
+  if( !G4TPC::ENABLE_DIRECT_LASER_HITS )
+    {
+      auto tpcclustercleaner = new TpcClusterCleaner;
+      tpcclustercleaner->Verbosity(verbosity);
+      tpcclustercleaner->set_cluster_version(G4TRACKING::cluster_version);
+      se->registerSubsystem(tpcclustercleaner);
+    }
+
+  // direct laser reconstruction
+  if( G4TPC::ENABLE_DIRECT_LASER_HITS )
+    { 
+      auto directLaserReconstruction = new TpcDirectLaserReconstruction;
+      directLaserReconstruction->set_outputfile( G4TPC::DIRECT_LASER_ROOTOUTPUT_FILENAME );
+      directLaserReconstruction->set_savehistograms( G4TPC::DIRECT_LASER_SAVEHISTOGRAMS );
+      directLaserReconstruction->set_histogram_outputfile( G4TPC::DIRECT_LASER_HISTOGRAMOUTPUT_FILENAME );
+      se->registerSubsystem(directLaserReconstruction); 
+    }
   
+  // central membrane reconstruction
+  if( G4TPC::ENABLE_CENTRAL_MEMBRANE_HITS )
+    {
+      // central membrane clusterizer
+      auto centralMembraneClusterizer = new PHTpcCentralMembraneClusterizer;
+      centralMembraneClusterizer->Verbosity(verbosity);
+      centralMembraneClusterizer->set_histos_on( false );
+      centralMembraneClusterizer->set_modulo_threshold(5);
+      centralMembraneClusterizer->set_metaCluster_threshold(18);
+      se->registerSubsystem(centralMembraneClusterizer);
+    
+
+      // match central membrane clusters to pads and generate distortion correction
+      auto centralMembraneMatcher = new PHTpcCentralMembraneMatcher;
+      centralMembraneMatcher->setSavehistograms( false );
+      centralMembraneMatcher->Verbosity( verbosity );
+      centralMembraneMatcher->setNMatchIter(2);
+      se->registerSubsystem(centralMembraneMatcher);
+    }
+
 }
 
 void Micromegas_Clustering()
