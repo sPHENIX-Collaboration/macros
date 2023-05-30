48b89767a2336c319b1957122bc7c9e2656b4c60
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-05-30 09:17:57 -0400

    fix typo

--- CDBTest/TestCDBInsert.C
+++ CDBTest/TestCDBInsert.C
@@ -8,6 +8,10 @@
 R__LOAD_LIBRARY(libsphenixnpc.so)
 R__LOAD_LIBRARY(libphool.so)
 
+// if you get errors like
+// Error inserting payload test.root, msg: "DataBaseException: Global Tag is locked."
+// use CDBUtils to unlock your global tag (unlockGlobalTag("<global tag>")
+
 void TestCDBInsert()
 {
   recoConsts *rc = recoConsts::instance();
@@ -16,8 +20,8 @@ void TestCDBInsert()
   CDBUtils *cdb = new CDBUtils(rc->get_StringFlag("CDB_GLOBALTAG"));
   cdb->createPayloadType("TestBeginValidity");
   cdb->insertPayload("TestBeginValidity","test.root",10);
-  cdb->createPayloadType("TestBeingEndValidity");
-  cdb->insertPayload("TestBeingEndValidity","test.root",10,20);
+  cdb->createPayloadType("TestBeginEndValidity");
+  cdb->insertPayload("TestBeginEndValidity","test.root",10,20);
   return;
 }
 
