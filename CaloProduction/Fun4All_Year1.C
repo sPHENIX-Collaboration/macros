6136dbb22ba64b065e5ed0043248491ffd9b4121
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-10-31 19:21:36 -0400

    set global tag to 2023p003

--- CaloProduction/Fun4All_Year1.C
+++ CaloProduction/Fun4All_Year1.C
@@ -61,7 +61,7 @@ void Fun4All_Year1(const std::string &fname = "/sphenix/lustre01/sphnxpro/commis
   //===============
   // ENABLE::CDB = true;
   // global tag
-  rc->set_StringFlag("CDB_GLOBALTAG", "2023p002");
+  rc->set_StringFlag("CDB_GLOBALTAG", "2023p003");
   // // 64 bit timestamp
   rc->set_uint64Flag("TIMESTAMP", runnumber);
   CDBInterface::instance()->Verbosity(1);
