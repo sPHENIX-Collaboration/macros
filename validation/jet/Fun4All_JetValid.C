9ec0f1ec3e34398f1859b264f302bc56ff489daa
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-05-14 15:12:18 -0400

    Create directory and macro skeleton

---
+++ validation/jet/Fun4All_JetValid.C
@@ -0,0 +1,19 @@
+#ifndef FUN4ALL_JETVALID_C
+#define FUN4ALL_JETVALID_C
+
+/* TODO includes will go here */
+/* TODO load your libraries here */
+
+// macro body -----------------------------------------------------------------
+
+void Fun4all_JetValid(
+  /* TODO arguments go here */
+) {
+
+  /* TODO fill the body here */
+
+}
+
+#endif
+
+// end ------------------------------------------------------------------------
