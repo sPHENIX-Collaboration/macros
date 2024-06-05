a3168d6456cfc4a391443103db35570a3f3a2d1e
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-06-05 16:42:40 -0400

    add ll1 to combiner

--- TriggerProduction/ll1_makelist.sh
+++ TriggerProduction/ll1_makelist.sh
@@ -15,9 +15,9 @@ fi
 type=$1
 runnumber=$(printf "%08d" $2)
 
-/bin/ls -1 /sphenix/lustre01/sphnxpro/physics/LL1/${type}/${type}_*-${runnumber}-* >ll1.list
-if [ ! -s ll1.list ]
+/bin/ls -1 /sphenix/lustre01/sphnxpro/physics/LL1/${type}/${type}_*-${runnumber}-* >seb19.list
+if [ ! -s seb19.list ]
 then
-  echo ll1.list empty, removing it
-  rm  ll1.list
+  echo seb19.list empty, removing it
+  rm  seb19.list
 fi
