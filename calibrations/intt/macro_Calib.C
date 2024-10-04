404a4740333fcbed8335981a620a69a0b3bba571
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-10-04 16:33:29 -0400

    fix calib macro to use file lists

--- calibrations/intt/macro_Calib.C
+++ calibrations/intt/macro_Calib.C
@@ -22,57 +22,77 @@ R__LOAD_LIBRARY(libffamodules.so)
 R__LOAD_LIBRARY(libfun4allraw.so)
 R__LOAD_LIBRARY(libffarawmodules.so)
 
-#include <filesystem>
-namespace fs = std::filesystem;
+bool isGood(const string &infile);
 
 void macro_Calib(
-    int run_num,
-    int num_evt,
-    std::string const &gl1_format,
-    std::string const &intt_format,
-    std::string const &hotmap_cdb_file,
-    std::string const &hotmap_png_file,
-    std::string const &bcomap_cdb_file,
-    std::string const &bcomap_png_file, 
+  int num_evt,
+  int run_num,
+  const std::string &hotmap_cdb_file = "hotmap.root",
+  const std::string &hotmap_png_file = "hotmap.png",
+  const std::string &bcomap_cdb_file = "bcomap.root",
+  const std::string &bcomap_png_file = "bcomap.png",
+  const string &input_gl1file = "gl1daq.list",
+  const string &input_inttfile00 = "intt0.list",
+  const string &input_inttfile01 = "intt1.list",
+  const string &input_inttfile02 = "intt2.list",
+  const string &input_inttfile03 = "intt3.list",
+  const string &input_inttfile04 = "intt4.list",
+  const string &input_inttfile05 = "intt5.list",
+  const string &input_inttfile06 = "intt6.list",
+  const string &input_inttfile07 = "intt7.list",
+
     bool isStreaming = true)
 {
-  char buff_gl1[256];
-  char buff[256];
   // GL1 which provides the beam clock reference (if we ran with GL1)
+  vector<string> gl1_infile;
+  gl1_infile.push_back(input_gl1file);
+
+// INTT
+  vector<string> intt_infile;
+  intt_infile.push_back(input_inttfile00);
+  intt_infile.push_back(input_inttfile01);
+  intt_infile.push_back(input_inttfile02);
+  intt_infile.push_back(input_inttfile03);
+  intt_infile.push_back(input_inttfile04);
+  intt_infile.push_back(input_inttfile05);
+  intt_infile.push_back(input_inttfile06);
+  intt_infile.push_back(input_inttfile07);
+
   Fun4AllServer *se = Fun4AllServer::instance();
-  se->Verbosity(1);
+//  se->Verbosity(1);
   // Input Manager
   recoConsts *rc = recoConsts::instance();
-  rc->set_StringFlag("CDB_GLOBALTAG", "ProdA_2023");
+  rc->set_StringFlag("CDB_GLOBALTAG", "ProdA_2024");
   rc->set_uint64Flag("TIMESTAMP", run_num);
 
   Fun4AllStreamingInputManager *in = new Fun4AllStreamingInputManager("Comb");
-  std::vector<std::string> missing_list_files = {};
-  InttOdbcQuery query;
-  query.Query(run_num);
-  if (query.IsStreaming()) // run 45235 -> GL1p offically established 
+  int i = 0;
+  for (auto iter : gl1_infile)
   {
-    SingleGl1PoolInput *gl1_sngl = new SingleGl1PoolInput("GL1");
-    snprintf(buff_gl1, sizeof(buff_gl1), gl1_format.c_str(), run_num);
-    std::cout << buff_gl1 << std::endl;
-    gl1_sngl->AddListFile(buff_gl1);
-    in->registerStreamingInput(gl1_sngl, InputManagerType::GL1);
-  } // If run is not streaming, doing combining without GL1 is also fine for INTT standalone calibration
-  for (int i = 0; i < 8; ++i)
-  {
-    snprintf(buff, sizeof(buff), intt_format.c_str(), run_num, i);
-    if (!std::filesystem::exists(buff))
+    if (isGood(iter))
     {
-      missing_list_files.push_back(buff);
-      continue;
+      SingleGl1PoolInput *gl1_sngl = new SingleGl1PoolInput("GL1_" + to_string(i));
+      //    gl1_sngl->Verbosity(3);
+      gl1_sngl->AddListFile(iter);
+      in->registerStreamingInput(gl1_sngl, InputManagerType::GL1);
+      i++;
     }
-
-    SingleInttPoolInput *intt_sngl = new SingleInttPoolInput("INTT_" + std::to_string(i));
-    intt_sngl->SetNegativeBco(120 - 23);
-    intt_sngl->SetBcoRange(120);
-    intt_sngl->AddListFile(buff);
-    in->registerStreamingInput(intt_sngl, InputManagerType::INTT);
   }
+  i = 0;
+    for (auto iter : intt_infile)
+    {
+      if (isGood(iter))
+      {
+	cout << "opening file " << iter << endl;
+	SingleInttPoolInput *intt_sngl = new SingleInttPoolInput("INTT_" + to_string(i));
+//    intt_sngl->Verbosity(3);
+	intt_sngl->SetNegativeBco(120-23);
+	intt_sngl->SetBcoRange(500);
+	intt_sngl->AddListFile(iter);
+	in->registerStreamingInput(intt_sngl, InputManagerType::INTT);
+	i++;
+      }
+    }
   se->registerInputManager(in);
 
   InttCalib *inttcalib = new InttCalib();
@@ -84,7 +104,7 @@ void macro_Calib(
   inttcalib->SetBcoMapCdbFile(bcomap_cdb_file);
   inttcalib->SetBcoMapPngFile(bcomap_png_file);
   inttcalib->SetBcoMaximumEvent(num_evt); // default = 50k
-  inttcalib->SetStreamingMode(query.IsStreaming());
+  inttcalib->SetStreamingMode(isStreaming);
   inttcalib->SetRunNumber(run_num);
 
   inttcalib->SetHotMapCdbFile(hotmap_cdb_file);
@@ -93,15 +113,26 @@ void macro_Calib(
   // inttcalib->SetBcoMapPngFile(bcomap_save_file);
 
   se->registerSubsystem(inttcalib);
-  if(inttcalib->Verbosity())
-  {
-    std::cout << "Run: " << run_num << " is " << (query.IsStreaming() ? "streaming" : "triggered") << std::endl;
-  }
-
   se->run(num_evt);
 
   se->End();
   delete se;
   cout << "all done" << endl;
   gSystem->Exit(0);
+}
+
+bool isGood(const string &infile)
+{
+  ifstream intest;
+  intest.open(infile);
+  bool goodfile = false;
+  if (intest.is_open())
+  {
+    if (intest.peek() != std::ifstream::traits_type::eof()) // is it non zero?
+    {
+      goodfile = true;
+    }
+      intest.close();
+  }
+  return goodfile;
 }
