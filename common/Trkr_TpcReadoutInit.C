9aaaad969b9f833390dc247e475dc4083a94e177
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2024-10-10 12:04:15 -0400

    changed default drift velocity for all Ar/CF4/iC4H10 runs, based on discussion with Tony, Xu-Dong.

--- common/Trkr_TpcReadoutInit.C
+++ common/Trkr_TpcReadoutInit.C
@@ -13,12 +13,21 @@ void TpcReadoutInit(const int RunNumber = 41989)
 {
 
   TRACKING::reco_tpc_is_configured = true;
-  if(RunNumber<45737){
-    G4TPC::tpc_drift_velocity_reco = (8.0 / 1000) * 107.0 / 105.0;
-  }else{ //change to Ar:cf4:N2 at run number 45737
-    G4TPC::tpc_drift_velocity_reco = (7.0 / 1000);
+  if(RunNumber<45737)
+  {
+    // all Ar/CF4 runs
+    G4TPC::tpc_drift_velocity_reco = (8.0 / 1000) * 107.0 / 105.0 // cm/ns;
+  } else if(RunNumber<49515) {
+    // Ar/CF4/N2 runs
+    G4TPC::tpc_drift_velocity_reco = 0.007; // cm/ns
+  } else {
+    // Ar/CF4/iC4H10
+    /* default drift velocity from Xu-Dong's study */
+    G4TPC::tpc_drift_velocity_reco = 0.00713; // cm/ns
   }
-  if(RunNumber>=41624){
+
+  if(RunNumber>=41624)
+  {
     TRACKING::reco_tpc_maxtime_sample = 425;
     TRACKING::reco_tpc_time_presample = 40;//120 - 80
   }else{
