ce8ec11a749982165a94dc87be2670d14814b7a1
xyu3 <xyu3@ssh.sdcc.bnl.gov>
2025-02-27 09:11:02 -0500

    fix static distortion flag for data

--- TrackingProduction/Fun4All_FieldOnAllTrackersCalos.C
+++ TrackingProduction/Fun4All_FieldOnAllTrackersCalos.C
@@ -143,7 +143,7 @@ void Fun4All_FieldOnAllTrackersCalos(
   G4TPC::ENABLE_MODULE_EDGE_CORRECTIONS = true;
   //to turn on the default static corrections, enable the two lines below
   //G4TPC::ENABLE_STATIC_CORRECTIONS = true;
-  //G4TPC::DISTORTIONS_USE_PHI_AS_RADIANS = false;
+  //G4TPC::USE_PHI_AS_RAD_STATIC_CORRECTIONS = false;
   //G4MAGNET::magfield = "0.01";
   G4MAGNET::magfield_tracking = G4MAGNET::magfield;
   G4MAGNET::magfield_rescale = 1;
--- TrackingProduction/Fun4All_TrackSeeding.C
+++ TrackingProduction/Fun4All_TrackSeeding.C
@@ -123,7 +123,7 @@ void Fun4All_TrackSeeding(
 
   // to turn on the default static corrections, enable the two lines below
   G4TPC::ENABLE_STATIC_CORRECTIONS = true;
-  G4TPC::DISTORTIONS_USE_PHI_AS_RADIANS = false;
+  G4TPC::USE_PHI_AS_RAD_STATIC_CORRECTIONS = false;
 
   //to turn on the average corrections, enable the three lines below
   //note: these are designed to be used only if static corrections are also applied
