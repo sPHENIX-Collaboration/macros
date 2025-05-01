2be84c74d5636631af72dc3416e0ab70fff73914
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-05-01 19:17:50 -0400

    add run23 (AuAu) settings

--- common/G4_RunSettings.C
+++ common/G4_RunSettings.C
@@ -9,26 +9,30 @@ void RunSettings(int runnumber, const std::string type = "")
 {
   switch (runnumber)
   {
-  case 21: // zero beam xing angle, mvtx rotated
+  case 21:  // zero beam xing angle, mvtx rotated
     Input::BEAM_CONFIGURATION = Input::pp_ZEROANGLE;
     Enable::MVTX_APPLYMISALIGNMENT = true;
     break;
-  case 22: // zero beam xing angle, mvtx rotated
+  case 22:  // 1.5mRad xing angle, mvtx rotated
     Input::BEAM_CONFIGURATION = Input::pp_COLLISION;
     Enable::MVTX_APPLYMISALIGNMENT = true;
-    break;    
-  case 24: // single particle sims
     break;
-  case 25:     // run 25 AuAu: field off
+  case 23:  // AuAu 1mRad xing angle, mvtx rotated
+    Input::BEAM_CONFIGURATION = Input::AA_COLLISION;
+    Enable::MVTX_APPLYMISALIGNMENT = true;
+    break;
+  case 24:  // single particle sims
+    break;
+  case 25:  // run 25 AuAu: field off
     G4MAGNET::magfield = "0";
     break;
-  case 26:     // run 26 ppg02 sims: field off, ppg02 beam settings
+  case 26:  // run 26 ppg02 sims: field off, ppg02 beam settings
     Input::BEAM_CONFIGURATION = Input::ppg02;
     Enable::MVTX_APPLYMISALIGNMENT = true;
     G4MAGNET::magfield = "0";
     std::cout << "use ppg02 settings" << std::endl;
     break;
-  case 27:     // run 27 ppg02 sims: field off, ppg02 beam settings 40% strangeness fraction
+  case 27:  // run 27 ppg02 sims: field off, ppg02 beam settings 40% strangeness fraction
     Input::BEAM_CONFIGURATION = Input::ppg02;
     Enable::MVTX_APPLYMISALIGNMENT = true;
     G4MAGNET::magfield = "0";
