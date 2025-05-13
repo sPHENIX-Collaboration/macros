51f99c288bbc001a786be89509d09bfb754cf579
Blair D. Seidlitz <bseidlitz@sphnxuser02.sdcc.bnl.gov>
2025-05-13 19:24:33 -0400

    adding hcal calib gen macros

---
+++ calibrations/calo/hcal_calib_year2/genTSCalib/get_tsFiles_flat.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+currDir=$PWD
+
+cosmicsFile=$1 #cosmics runs
+tsFile=$2      #file to write merged TS root files to
+mergeNum=$3    #how many TS files to merge after given cosmics run #
+
+dummyFile="dummy.txt"  #file to hold first 'x' files after cosmics run # - sort of a staging file to ensure we got right files
+tag="cosmics"          #when searching for merged ts files, make sure to look for "merged_XXXXX.root" and not the merged root files with "cosmics" in name - given we use comsics in the name for this scripts output
+
+while IFS= read -r line
+do
+
+    echo "looking at run " 
+    echo "${IFS}" 
+    x=0  #counter
+    incr=$line
+    
+    #path to LCE output files
+    cd /sphenix/u/bseidlitz/work/macros/calibrations/calo/hcal_towerSlope_y2/cos_only_tsc_spec_output2/ 
+
+    touch $dummyFile
+
+    while [ $x -lt $mergeNum ]
+    do
+	
+	incr=$(($incr+1))
+
+        if [ $incr -gt 60000 ]; then
+            echo "Incr has exceeded 6000, exiting loop."
+            break
+        fi
+
+
+	FILE=`find ~+ -name "*$incr*" -not -name "*$tag*" -print -quit`
+
+	if [[ -a $FILE ]]
+	then
+	    echo $FILE >> $dummyFile
+	    x=$((x+1))
+	fi
+
+    done
+
+    echo "Merging these files..."
+    cat $dummyFile  
+
+    #merge files in dummyFile and send to new text file
+    string="merged/mergedTS_cosmics_$line.root"
+
+    rm $string 
+    bash -c "hadd -f $string @$dummyFile"
+
+    echo "$PWD/$string" >> $tsFile
+
+    rm $dummyFile
+
+done < $cosmicsFile
+
+mv $tsFile $currDir
+
+#execute fitting macro
+cd $currDir
+root.exe -b -q run_cosmicsTS.C\(\"$cosmicsFile\",\"$tsFile\"\) 
+
+echo "Finished running get_ts_files.sh."
+echo "All done!"
