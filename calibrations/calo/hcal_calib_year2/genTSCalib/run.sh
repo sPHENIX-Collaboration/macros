da4e1df843a9a074f29bcf9cf7c1fd1d896f58be
Anjaly Sasikumar Menon <anjsmenon@sphnxuser01.sdcc.bnl.gov>
2025-06-18 06:27:04 -0400

    Add flags to run HCALOUT or HCALIN analysis

--- calibrations/calo/hcal_calib_year2/genTSCalib/run.sh
+++ calibrations/calo/hcal_calib_year2/genTSCalib/run.sh
@@ -1,3 +1,8 @@
 # This performs the hadd-ing, fitting, and combining with a cosmics of
 # a set TSC spectra files.  See get_tsFiles_flat.sh for meaning of args
-sh get_tsFiles_flat.sh cos_runs.txt tsc_merge_list.txt 10
+
+
+run_ohcal=$1  # first argument: true/false for HCALOUT
+run_ihcal=$2  # second argument: true/false for HCALIN
+
+sh get_tsFiles_flat.sh cos_runs.txt tsc_merge_list.txt 2 $run_ohcal $run_ihcal
