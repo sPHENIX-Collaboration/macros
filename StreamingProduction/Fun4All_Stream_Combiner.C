52537c9c1e1ea73eac6636adb9332e4d311514d3
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-10-24 15:50:46 -0400

    remove obsolete InttOdbcQuery

--- StreamingProduction/Fun4All_Stream_Combiner.C
+++ StreamingProduction/Fun4All_Stream_Combiner.C
@@ -1,20 +1,19 @@
 #include <GlobalVariables.C>
 #include <Trkr_TpcReadoutInit.C>
 
-
 #include <ffarawmodules/InttCheck.h>
 #include <ffarawmodules/StreamingCheck.h>
 #include <ffarawmodules/TpcCheck.h>
 
-#include <ffamodules/HeadReco.h>
 #include <ffamodules/FlagHandler.h>
+#include <ffamodules/HeadReco.h>
 #include <ffamodules/SyncReco.h>
 
 #include <fun4allraw/Fun4AllStreamingInputManager.h>
 #include <fun4allraw/InputManagerType.h>
 #include <fun4allraw/SingleGl1PoolInput.h>
-#include <fun4allraw/SingleInttPoolInput.h>
 #include <fun4allraw/SingleInttEventInput.h>
+#include <fun4allraw/SingleInttPoolInput.h>
 #include <fun4allraw/SingleMicromegasPoolInput.h>
 #include <fun4allraw/SingleMvtxPoolInput.h>
 #include <fun4allraw/SingleTpcPoolInput.h>
@@ -33,57 +32,57 @@ R__LOAD_LIBRARY(libfun4allraw.so)
 R__LOAD_LIBRARY(libffarawmodules.so)
 R__LOAD_LIBRARY(libintt.so)
 
-bool isGood(const string &infile);
+bool isGood(const std::string &infile);
 int getrunnumber(const std::string &listfile);
 
 void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
-                             const string &input_gl1file = "gl1daq.list",
-                             const string &input_inttfile00 = "intt0.list",
-                             const string &input_inttfile01 = "intt1.list",
-                             const string &input_inttfile02 = "intt2.list",
-                             const string &input_inttfile03 = "intt3.list",
-                             const string &input_inttfile04 = "intt4.list",
-                             const string &input_inttfile05 = "intt5.list",
-                             const string &input_inttfile06 = "intt6.list",
-                             const string &input_inttfile07 = "intt7.list",
-                             const string &input_mvtxfile00 = "mvtx0.list",
-                             const string &input_mvtxfile01 = "mvtx1.list",
-                             const string &input_mvtxfile02 = "mvtx2.list",
-                             const string &input_mvtxfile03 = "mvtx3.list",
-                             const string &input_mvtxfile04 = "mvtx4.list",
-                             const string &input_mvtxfile05 = "mvtx5.list",
-                             const string &input_tpcfile00 = "tpc00.list",
-                             const string &input_tpcfile01 = "tpc01.list",
-                             const string &input_tpcfile02 = "tpc02.list",
-                             const string &input_tpcfile03 = "tpc03.list",
-                             const string &input_tpcfile04 = "tpc04.list",
-                             const string &input_tpcfile05 = "tpc05.list",
-                             const string &input_tpcfile06 = "tpc06.list",
-                             const string &input_tpcfile07 = "tpc07.list",
-                             const string &input_tpcfile08 = "tpc08.list",
-                             const string &input_tpcfile09 = "tpc09.list",
-                             const string &input_tpcfile10 = "tpc10.list",
-                             const string &input_tpcfile11 = "tpc11.list",
-                             const string &input_tpcfile12 = "tpc12.list",
-                             const string &input_tpcfile13 = "tpc13.list",
-                             const string &input_tpcfile14 = "tpc14.list",
-                             const string &input_tpcfile15 = "tpc15.list",
-                             const string &input_tpcfile16 = "tpc16.list",
-                             const string &input_tpcfile17 = "tpc17.list",
-                             const string &input_tpcfile18 = "tpc18.list",
-                             const string &input_tpcfile19 = "tpc19.list",
-                             const string &input_tpcfile20 = "tpc20.list",
-                             const string &input_tpcfile21 = "tpc21.list",
-                             const string &input_tpcfile22 = "tpc22.list",
-                             const string &input_tpcfile23 = "tpc23.list",
-                             const string &input_tpotfile = "tpot.list")
+                             const std::string &input_gl1file = "gl1daq.list",
+                             const std::string &input_inttfile00 = "intt0.list",
+                             const std::string &input_inttfile01 = "intt1.list",
+                             const std::string &input_inttfile02 = "intt2.list",
+                             const std::string &input_inttfile03 = "intt3.list",
+                             const std::string &input_inttfile04 = "intt4.list",
+                             const std::string &input_inttfile05 = "intt5.list",
+                             const std::string &input_inttfile06 = "intt6.list",
+                             const std::string &input_inttfile07 = "intt7.list",
+                             const std::string &input_mvtxfile00 = "mvtx0.list",
+                             const std::string &input_mvtxfile01 = "mvtx1.list",
+                             const std::string &input_mvtxfile02 = "mvtx2.list",
+                             const std::string &input_mvtxfile03 = "mvtx3.list",
+                             const std::string &input_mvtxfile04 = "mvtx4.list",
+                             const std::string &input_mvtxfile05 = "mvtx5.list",
+                             const std::string &input_tpcfile00 = "tpc00.list",
+                             const std::string &input_tpcfile01 = "tpc01.list",
+                             const std::string &input_tpcfile02 = "tpc02.list",
+                             const std::string &input_tpcfile03 = "tpc03.list",
+                             const std::string &input_tpcfile04 = "tpc04.list",
+                             const std::string &input_tpcfile05 = "tpc05.list",
+                             const std::string &input_tpcfile06 = "tpc06.list",
+                             const std::string &input_tpcfile07 = "tpc07.list",
+                             const std::string &input_tpcfile08 = "tpc08.list",
+                             const std::string &input_tpcfile09 = "tpc09.list",
+                             const std::string &input_tpcfile10 = "tpc10.list",
+                             const std::string &input_tpcfile11 = "tpc11.list",
+                             const std::string &input_tpcfile12 = "tpc12.list",
+                             const std::string &input_tpcfile13 = "tpc13.list",
+                             const std::string &input_tpcfile14 = "tpc14.list",
+                             const std::string &input_tpcfile15 = "tpc15.list",
+                             const std::string &input_tpcfile16 = "tpc16.list",
+                             const std::string &input_tpcfile17 = "tpc17.list",
+                             const std::string &input_tpcfile18 = "tpc18.list",
+                             const std::string &input_tpcfile19 = "tpc19.list",
+                             const std::string &input_tpcfile20 = "tpc20.list",
+                             const std::string &input_tpcfile21 = "tpc21.list",
+                             const std::string &input_tpcfile22 = "tpc22.list",
+                             const std::string &input_tpcfile23 = "tpc23.list",
+                             const std::string &input_tpotfile = "tpot.list")
 {
-// GL1 which provides the beam clock reference (if we ran with GL1)
-  vector<string> gl1_infile;
+  // GL1 which provides the beam clock reference (if we ran with GL1)
+  std::vector<std::string> gl1_infile;
   gl1_infile.push_back(input_gl1file);
 
-// MVTX
-  vector<string> mvtx_infile;
+  // MVTX
+  std::vector<std::string> mvtx_infile;
   mvtx_infile.push_back(input_mvtxfile00);
   mvtx_infile.push_back(input_mvtxfile01);
   mvtx_infile.push_back(input_mvtxfile02);
@@ -91,8 +90,8 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   mvtx_infile.push_back(input_mvtxfile04);
   mvtx_infile.push_back(input_mvtxfile05);
 
-// INTT
-  vector<string> intt_infile;
+  // INTT
+  std::vector<std::string> intt_infile;
   intt_infile.push_back(input_inttfile00);
   intt_infile.push_back(input_inttfile01);
   intt_infile.push_back(input_inttfile02);
@@ -102,7 +101,7 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   intt_infile.push_back(input_inttfile06);
   intt_infile.push_back(input_inttfile07);
 
-  vector<string> tpc_infile;
+  std::vector<std::string> tpc_infile;
   tpc_infile.push_back(input_tpcfile00);
   tpc_infile.push_back(input_tpcfile01);
   tpc_infile.push_back(input_tpcfile02);
@@ -128,8 +127,8 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   tpc_infile.push_back(input_tpcfile22);
   tpc_infile.push_back(input_tpcfile23);
 
-// TPOT
-  vector<string> tpot_infile;
+  // TPOT
+  std::vector<std::string> tpot_infile;
   tpot_infile.push_back(input_tpotfile);
 
   int runnumber = -99999;
@@ -160,19 +159,19 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
     gSystem->Exit(1);
   }
 
-  TpcSampleInit( runnumber );
-  std::cout<< " run: " << runnumber
-	   << " samples: " << TRACKING::reco_tpc_maxtime_sample
-	   << " pre: " << TRACKING::reco_tpc_time_presample
-	   << std::endl;
+  TpcSampleInit(runnumber);
+  std::cout << " run: " << runnumber
+            << " samples: " << TRACKING::reco_tpc_maxtime_sample
+            << " pre: " << TRACKING::reco_tpc_time_presample
+            << std::endl;
 
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(1);
   recoConsts *rc = recoConsts::instance();
   Fun4AllStreamingInputManager *in = new Fun4AllStreamingInputManager("Comb");
-//  in->Verbosity(3);
+  //  in->Verbosity(3);
 
-// create and register input managers
+  // create and register input managers
   int NumInputs = 0;
   int i = 0;
 
@@ -180,7 +179,7 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   {
     if (isGood(iter))
     {
-      SingleGl1PoolInput *gl1_sngl = new SingleGl1PoolInput("GL1_" + to_string(i));
+      SingleGl1PoolInput *gl1_sngl = new SingleGl1PoolInput("GL1_" + std::to_string(i));
       //    gl1_sngl->Verbosity(3);
       gl1_sngl->AddListFile(iter);
       in->registerStreamingInput(gl1_sngl, InputManagerType::GL1);
@@ -194,9 +193,9 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   {
     if (isGood(iter))
     {
-      cout << "opening file " << iter << endl;
-      SingleInttPoolInput *intt_sngl = new SingleInttPoolInput("INTT_" + to_string(i));
-//    intt_sngl->Verbosity(3);
+      std::cout << "opening file " << iter << std::endl;
+      SingleInttPoolInput *intt_sngl = new SingleInttPoolInput("INTT_" + std::to_string(i));
+      //    intt_sngl->Verbosity(3);
       intt_sngl->AddListFile(iter);
       in->registerStreamingInput(intt_sngl, InputManagerType::INTT);
       i++;
@@ -209,8 +208,8 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   {
     if (isGood(iter))
     {
-      SingleMvtxPoolInput *mvtx_sngl = new SingleMvtxPoolInput("MVTX_" + to_string(i));
-      //mvtx_sngl->Verbosity(5);
+      SingleMvtxPoolInput *mvtx_sngl = new SingleMvtxPoolInput("MVTX_" + std::to_string(i));
+      // mvtx_sngl->Verbosity(5);
       mvtx_sngl->AddListFile(iter);
       in->registerStreamingInput(mvtx_sngl, InputManagerType::MVTX);
       i++;
@@ -223,12 +222,12 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   {
     if (isGood(iter))
     {
-      SingleTpcPoolInput *tpc_sngl = new SingleTpcPoolInput("TPC_" + to_string(i));
-//    tpc_sngl->Verbosity(2);
+      SingleTpcPoolInput *tpc_sngl = new SingleTpcPoolInput("TPC_" + std::to_string(i));
+      //    tpc_sngl->Verbosity(2);
       //   tpc_sngl->DryRun();
       tpc_sngl->SetBcoRange(5);
       tpc_sngl->AddListFile(iter);
-//    tpc_sngl->SetMaxTpcTimeSamples(TRACKING::reco_tpc_maxtime_sample);
+      //    tpc_sngl->SetMaxTpcTimeSamples(TRACKING::reco_tpc_maxtime_sample);
       tpc_sngl->SetMaxTpcTimeSamples(1024);
       in->registerStreamingInput(tpc_sngl, InputManagerType::TPC);
       i++;
@@ -241,7 +240,7 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   {
     if (isGood(iter))
     {
-      SingleMicromegasPoolInput *mm_sngl = new SingleMicromegasPoolInput("MICROMEGAS_" + to_string(i));
+      SingleMicromegasPoolInput *mm_sngl = new SingleMicromegasPoolInput("MICROMEGAS_" + std::to_string(i));
       //   sngl->Verbosity(3);
       mm_sngl->SetBcoRange(10);
       mm_sngl->SetNegativeBco(2);
@@ -253,7 +252,7 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
   }
   NumInputs += i;
 
-// if there is no input manager this macro will still run - so just quit here
+  // if there is no input manager this macro will still run - so just quit here
   if (NumInputs == 0)
   {
     std::cout << "no file lists no input manager registered, quitting" << std::endl;
@@ -275,8 +274,8 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
 
   FlagHandler *flag = new FlagHandler();
   se->registerSubsystem(flag);
-  // string outdir = "/sphenix/user/pinkenbu";
-  // string outfilename = "./gl1A.root";
+  // std::string outdir = "/sphenix/user/pinkenbu";
+  // std::string outfilename = "./gl1A.root";
   // int neventsper = 1;
   Fun4AllOutputManager *out = new Fun4AllDstOutputManager("out", "streaming.root");
 
@@ -295,18 +294,18 @@ void Fun4All_Stream_Combiner(int nEvents = 5, int runnumber_unused = 41989,
 
   se->End();
   delete se;
-  cout << "all done" << endl;
+  std::cout << "all done" << std::endl;
   gSystem->Exit(0);
 }
 
-bool isGood(const string &infile)
+bool isGood(const std::string &infile)
 {
-  ifstream intest;
+  std::ifstream intest;
   intest.open(infile);
   bool goodfile = false;
   if (intest.is_open())
   {
-    if (intest.peek() != std::ifstream::traits_type::eof()) // is it non zero?
+    if (intest.peek() != std::ifstream::traits_type::eof())  // is it non zero?
     {
       goodfile = true;
     }
