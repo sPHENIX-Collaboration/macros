be67891fb78bc463bef6a08a0fc6e9085acb1294
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-11-01 10:39:25 -0400

    clang-tidy, cppcheck for BcoQA/condor

--- BcoQA/Fun4All_Gl1QA.C
+++ BcoQA/Fun4All_Gl1QA.C
@@ -8,6 +8,7 @@
 #include <Rtypes.h> // defines R__LOAD_LIBRARY macro for clang-tidy
 #include <TSystem.h>
 
+// cppcheck-suppress unknownMacro
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libfun4allraw.so)
 R__LOAD_LIBRARY(libffarawmodules.so)
