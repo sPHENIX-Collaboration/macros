e6e5d764381116d300bf521979df826ab3f9c67b
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-11-01 11:09:11 -0400

    fix include file order

--- calibrations/mbd/Fun4All_MBD_CalPass.C
+++ calibrations/mbd/Fun4All_MBD_CalPass.C
@@ -13,11 +13,11 @@
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllUtils.h>
 #include <fun4all/Fun4AllInputManager.h>
-
-#include <fun4allraw/Fun4AllPrdfInputManager.h>
 #include <fun4all/Fun4AllDstInputManager.h>
 #include <fun4all/Fun4AllOutputManager.h>
 #include <fun4all/Fun4AllDstOutputManager.h>
+
+#include <fun4allraw/Fun4AllPrdfInputManager.h>
 #include <fun4allraw/Fun4AllEventOutputManager.h>
 
 #include <phool/recoConsts.h>
