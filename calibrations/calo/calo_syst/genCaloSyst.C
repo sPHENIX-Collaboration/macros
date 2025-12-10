2b2dd86100cb0172b5bec2c5925438d10b93ff18
emclaughlin2 <emlaugh1020@gmail.com>
2025-12-10 17:42:30 -0500

    Add hadronic response uncertainty to calo systematics. Add cdb hist macro.

--- calibrations/calo/calo_syst/genCaloSyst.C
+++ calibrations/calo/calo_syst/genCaloSyst.C
@@ -1,8 +1,6 @@
-#include "cdbHistConv.C"
+#include <cdbHistConv.C>
 
 void genCaloSyst(){
-
-
   //string calo = "CEMC";
   //string calo = "HCALOUT";
   //string calo = "HCALIN";
@@ -15,15 +13,17 @@ void genCaloSyst(){
     int begin = 46000;
     int end   = 53862;
   
-    float tbyt_stat,shift,sys_v1,sys_v2;
+    float tbyt_stat,shift,sys_v1,sys_v2,had_frac;
     int eta,phi;
     bool embool;
+    float had_resp_unc = 0.117; // value from test beam
   
     if(calo=="CEMC"){
       tbyt_stat = 0.03; // tower by tower stat uncertainty 
       shift = 0.026; // global scale shift  
       sys_v1 = 0.02;
       sys_v2 = 0.02;
+      had_frac = 0.61;
       embool = 0;
       eta = 96;
       phi = 256;
@@ -33,6 +33,7 @@ void genCaloSyst(){
       shift = 0.01; // global scale shift  
       sys_v1 = 0.02;
       sys_v2 = 0.02;
+      had_frac = 0.972;
       embool = 1;
       eta = 24;
       phi = 64;
@@ -42,6 +43,7 @@ void genCaloSyst(){
       shift = 0.1; // global scale shift  
       sys_v1 = 0.02;
       sys_v2 = 0.05;
+      had_frac = 0.961;
       embool = 1;
       eta = 24;
       phi = 64;
@@ -52,6 +54,7 @@ void genCaloSyst(){
     TH2F* h_shift     = (TH2F*) h_sys->Clone("h_shift");
     TH2F* h_v1        = (TH2F*) h_sys->Clone("h_v1");
     TH2F* h_v2        = (TH2F*) h_sys->Clone("h_v2");
+    TH2F* h_had_resp  = (TH2F*) h_sys->Clone("h_had_resp");
   
     TRandom3* rnd = new TRandom3();
   
@@ -64,8 +67,12 @@ void genCaloSyst(){
         h_v1->SetBinContent(ie,ip,v1);
         float v2 = 1.0 + sys_v1*TMath::Cos(2.0*(float) ip/phi*2.*TMath::Pi()); 
         h_v2->SetBinContent(ie,ip,v2);
+        float had_resp = 1.0 + 2.0*had_resp_unc*had_frac/(sqrt(12));
+        std::cout << had_resp << " ";
+        h_had_resp->SetBinContent(ie,ip,had_resp);
       }
     }
+    std::cout << std::endl;
   
     
         
@@ -73,10 +80,28 @@ void genCaloSyst(){
     histToCaloCDBTree(Form("cdbFiles/%s_shift_syst_%d_%d.root"       ,calo.c_str(),begin,end),"calo_sys", embool, h_shift);
     histToCaloCDBTree(Form("cdbFiles/%s_v1Modulation_syst_%d_%d.root",calo.c_str(),begin,end),"calo_sys", embool, h_v1);
   //  histToCaloCDBTree(Form("cdbFilesAuAu/%s_v2Modulation_syst_%d_%d.root",calo.c_str(),begin,end),"calo_sys", embool, h_v2);
+    histToCaloCDBTree(Form("cdbFiles/%s_had_resp_syst_%d_%d.root"     ,calo.c_str(),begin,end),"calo_sys", embool, h_had_resp);
   
-  };
+    TCanvas* c1 = new TCanvas("c1","c1",600,600);
+    h_had_resp->Draw("COLZ");
+    c1->Draw();
+    c1->SaveAs(Form("h_had_resp_%s.png",calo.c_str()));
 
-  //TCanvas* c1 = new TCanvas("c1","c1",600,600);
-  //h_v1->Draw("COLZ");
+    TCanvas* c2 = new TCanvas("c2","c2",600,600);
+    h_shift->Draw("COLZ");
+    c2->Draw();
+    c2->SaveAs(Form("h_shift_%s.png",calo.c_str()));
+
+    TCanvas* c3 = new TCanvas("c3","c3",600,600);
+    h_stat->Draw("COLZ");
+    c3->Draw();
+    c3->SaveAs(Form("h_stat_%s.png",calo.c_str()));
+
+    TCanvas* c4 = new TCanvas("c4","c4",600,600);
+    h_v1->Draw("COLZ");
+    c4->Draw();
+    c4->SaveAs(Form("h_v1_%s.png",calo.c_str()));
+
+  };
 
 }
