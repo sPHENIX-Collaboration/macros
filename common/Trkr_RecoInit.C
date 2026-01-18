f52157cc8dd527891924cdece9beceed459b512d
Joe Osborn <osbornjd91@gmail.com>
2026-01-18 13:46:25 -0500

    fix crash

--- common/Trkr_RecoInit.C
+++ common/Trkr_RecoInit.C
@@ -6,6 +6,7 @@
 #include <G4_ActsGeom.C>
 #include <G4_TrkrVariables.C>
 
+#include <fun4all/Fun4AllRunNodeInputManager.h>
 #include <tpc/TpcLoadDistortionCorrection.h>
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wundefined-internal"
@@ -37,7 +38,6 @@ void TrackingInit()
   // space charge correction
   if (G4TPC::ENABLE_MODULE_EDGE_CORRECTIONS || G4TPC::ENABLE_STATIC_CORRECTIONS || G4TPC::ENABLE_AVERAGE_CORRECTIONS)
   {
-    auto *se = Fun4AllServer::instance();
     auto *tpcLoadDistortionCorrection = new TpcLoadDistortionCorrection;
 
     tpcLoadDistortionCorrection->set_read_phi_as_radians(TpcLoadDistortionCorrection::DistortionType_Static, G4TPC::USE_PHI_AS_RAD_STATIC_CORRECTIONS);
