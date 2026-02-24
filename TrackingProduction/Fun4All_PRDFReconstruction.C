6043a925656ee764d10b6d62549be19a861d4e9d
Joe Osborn <osbornjd91@gmail.com>
2026-02-23 22:16:27 -0500

    add distortion corrections

--- TrackingProduction/Fun4All_PRDFReconstruction.C
+++ TrackingProduction/Fun4All_PRDFReconstruction.C
@@ -218,7 +218,18 @@ void Fun4All_PRDFReconstruction(
   TRACKING::pp_mode = true;
   G4TRACKING::convert_seeds_to_svtxtracks = false;
   G4TPC::REJECT_LASER_EVENTS = true;
+  
+  // to turn on the default static corrections, enable the two lines below
+  G4TPC::ENABLE_STATIC_CORRECTIONS = true;
+  G4TPC::USE_PHI_AS_RAD_STATIC_CORRECTIONS = false;
 
+  //to turn on the average corrections, enable the three lines below
+  //note: these are designed to be used only if static corrections are also applied
+  G4TPC::ENABLE_AVERAGE_CORRECTIONS = true;
+  G4TPC::USE_PHI_AS_RAD_AVERAGE_CORRECTIONS = true;
+   // to use a custom file instead of the database file:
+  G4TPC::average_correction_filename = CDBInterface::instance()->getUrl("TPC_LAMINATION_FIT_CORRECTION");
+  
   Enable::MVTX_APPLYMISALIGNMENT = true;
   ACTSGEOM::mvtx_applymisalignment = Enable::MVTX_APPLYMISALIGNMENT;
   TpcReadoutInit(runnumber);
