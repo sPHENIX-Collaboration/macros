9d99e0f0540fa6a7a8106b2b16b5c35edd95afd8
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2026-08-28 10:40:15 -0400

    in Tracking_Reco_TrackFit_run2pp() restore using SC_USE_MICROMEGAS to decide whether micromegas are used in ACTS fitter. set SC_USE_MICROMEGAS to false by default. This should not change the default acts fitter behavior, while allowing user to turn using Micromegas back on in steering macro.

--- common/G4_TrkrVariables.C
+++ common/G4_TrkrVariables.C
@@ -229,8 +229,8 @@ namespace G4TPC
 namespace G4TRACKING
 {
   // Space Charge calibration flag
-  bool SC_CALIBMODE = false;  // this is anded with G4TPC::ENABLE_DISTORTIONS in TrackingInit()
-  bool SC_USE_MICROMEGAS = true;
+  bool SC_CALIBMODE = false;
+  bool SC_USE_MICROMEGAS = false;
   double SC_COLLISIONRATE = 50e3;                                      // leave at 50 KHz for now, scaling of distortion map not implemented yet
   std::string SC_ROOTOUTPUT_FILENAME = "TpcSpaceChargeMatrices.root";  // space charge calibration output file
 
