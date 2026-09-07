cbb2a689a4faa7e913b6b2ba7cd790d318ec6f9a
Chris Pinkenburg <pinkenburg@bnl.gov>
2026-09-07 14:30:35 -0400

    fix comment

--- CDBTest/TestCDBRead.C
+++ CDBTest/TestCDBRead.C
@@ -15,10 +15,9 @@ R__LOAD_LIBRARY(libphool.so)
 void TestCDBRead()
 {
   recoConsts *rc = recoConsts::instance();
-  // please choose a unique name, if it is your username it's easier to see who created it
+// this is our floating cdb tag which contains all up to date data calibrations
   rc->set_StringFlag("CDB_GLOBALTAG", "newcdbtag");
   rc->set_uint64Flag("TIMESTAMP", 61272);
-  // 1000000 is the insert timestamp. Higher timestamps work, lower time stamps do not
   CDBInterface *cdb = CDBInterface::instance();
   std::cout << cdb->getUrl("Tracking_Geometry") << std::endl;
   gSystem->Exit(0);
